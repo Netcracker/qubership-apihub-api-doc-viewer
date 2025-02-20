576361617a06dec9c7f5a8bad911fe30be6e8964
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2025-02-20 16:31:24 +0000

    deploy: a3d9e062349547e2ab90d8d484dba5c75b5e9fbe

--- feature/rename-label-semi-breaking-changes/assets/graph-api-transformers-2d16c6ce.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./ErrorBoundaryFallback-544b3552.js";import{b as a}from"./buildASTSchema-5b3e7609.js";function t(o){return r(a(o,{noLocation:!0}))}export{t as b};
---
+++ feature/rename-label-semi-breaking-changes/assets/graph-api-transformers-f14be3db.js
@@ -0,0 +1,1 @@
+import{b as r}from"./ErrorBoundaryFallback-93124e20.js";import{b as a}from"./buildASTSchema-acce225c.js";function t(o){return r(a(o,{noLocation:!0}))}export{t as b};
