944faafad6c608f40e007e06ad1cfee461f05e6f
b41ex <b41ex@users.noreply.github.com>
2025-04-10 08:24:55 +0000

    deploy: 6c652ff8cb8e295efcf3958a85a7a268d313255a

---
+++ main/assets/GraphApiTest.stories-8cbfaaad.js
@@ -0,0 +1,51 @@
+import{G as l}from"./GraphQLOperationViewer-6322f861.js";import{g as m}from"./helpers-f21dd9b2.js";import{a as d}from"./preprocess-debb489c.js";import"./jsx-runtime-beaa2906.js";import"./index-f46741a2.js";import"./ErrorBoundaryFallback-4397c758.js";import"./index-415bee12.js";import"./buildASTSchema-535a766a.js";const G={title:"Graph Api Viewer",component:l,parameters:{},argTypes:{},args:{source:{}}},e={args:{source:{},expandedDepth:100}},r={args:{source:d({source:m`
+        type Query {
+          test: Union
+        }
+        union Union = String | ID | MyType
+        type MyType {
+          id: ID!
+        }
+      `})}},n={args:{source:d({source:m`
+        type Query {
+          test: CycledEntity
+        }
+        type CycledEntity {
+          value: String
+          child: CycledEntity
+        }
+      `,circular:!0})}};var t,a,p;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    source: {},
+    expandedDepth: 100
+  }
+}`,...(p=(a=e.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};var s,o,c;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    source: prepareGraphApiSchema({
+      source: graphapi\`
+        type Query {
+          test: Union
+        }
+        union Union = String | ID | MyType
+        type MyType {
+          id: ID!
+        }
+      \`
+    })
+  }
+}`,...(c=(o=r.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var i,y,u;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    source: prepareGraphApiSchema({
+      source: graphapi\`
+        type Query {
+          test: CycledEntity
+        }
+        type CycledEntity {
+          value: String
+          child: CycledEntity
+        }
+      \`,
+      circular: true
+    })
+  }
+}`,...(u=(y=n.parameters)==null?void 0:y.docs)==null?void 0:u.source}}};const U=["Test","Union","SelfCycled"];export{n as SelfCycled,e as Test,r as Union,U as __namedExportsOrder,G as default};
---
+++ main/assets/helpers-f21dd9b2.js
@@ -0,0 +1,1 @@
+import{b as o}from"./ErrorBoundaryFallback-4397c758.js";import{b as i}from"./buildASTSchema-535a766a.js";function a(r){return o(i(r,{noLocation:!0}))}function u(r){return a(r[0])}export{u as g};
