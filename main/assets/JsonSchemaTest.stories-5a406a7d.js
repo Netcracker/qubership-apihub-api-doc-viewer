a76f10049f23563dac5582913764da267677240b
b41ex <b41ex@users.noreply.github.com>
2025-06-06 07:17:32 +0000

    deploy: f3ddb9027934ab29091118f1afea074f05c55ba2

---
+++ main/assets/JsonSchemaTest.stories-5a406a7d.js
@@ -0,0 +1,5 @@
+import{J as o}from"./JsonSchemaViewer-b93e528e.js";import"./jsx-runtime-beaa2906.js";import"./index-f46741a2.js";import"./ErrorBoundaryFallback-472fa99a.js";import"./index-415bee12.js";import"./JsonPropNodeViewer-5b1c3bd9.js";const d={title:"Json Schema Viewer",component:o,parameters:{},argTypes:{},args:{schema:{}}},e={args:{schema:{}}};var r,s,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    schema: {}
+  }
+}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const g=["Test"];export{e as Test,g as __namedExportsOrder,d as default};
