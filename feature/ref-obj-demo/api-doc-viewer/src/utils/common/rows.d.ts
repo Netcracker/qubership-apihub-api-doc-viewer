160ed43345ce79f9adf425bbeaa756bc20dc04a9
makeev-pavel <makeev-pavel@users.noreply.github.com>
2025-08-13 11:07:18 +0000

    deploy: 6e61f1854af86ec8259e189dfa7639fd06b25d91

---
+++ feature/ref-obj-demo/api-doc-viewer/src/utils/common/rows.d.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2024-2025 NetCracker Technology Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { IModelTreeNode } from '@netcracker/qubership-apihub-api-data-model';
+export declare function stringifyItem(item: unknown | undefined): string | undefined;
+export declare function handleSeriesItem(key: string, item: unknown | undefined): string | undefined;
+export declare function isSeriesItemEmpty(item: unknown, replacedItem: unknown): boolean;
+export declare function listContainsNodeKind(node: IModelTreeNode<unknown, string, unknown> | null | undefined, ...kinds: string[]): boolean;
+export declare function listContainsNodeParentKind(node: IModelTreeNode<unknown, string, unknown> | null | undefined, ...kinds: string[]): boolean;
---
+++ feature/ref-obj-demo/assets/graph-api-transformers-8a6e346f.js
@@ -0,0 +1,1 @@
+import{b as r}from"./ErrorBoundaryFallback-36647b4c.js";import{b as a}from"./buildASTSchema-dbc456cb.js";function t(o){return r(a(o,{noLocation:!0}))}export{t as b};
