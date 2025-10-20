a676ce0d1fc58d5e220721b920c27a47d2baca03
JayLim2 <JayLim2@users.noreply.github.com>
2025-10-20 13:28:58 +0000

    deploy: fef94f46579d11a02dbe7c6f92fca0e34daca9bf

---
+++ feature/lazy-tree-building/assets/JsonSchemaDiffTest.stories-7b01eb11.js
@@ -0,0 +1,289 @@
+import{S as n,D as e,a as t}from"./ErrorBoundaryFallback-1f8fbe38.js";import{J as M}from"./JsonSchemaDiffViewer-386424bc.js";import{b as i,R as s}from"./preprocess-142fb67d.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./index-415bee12.js";import"./JsonPropNodeViewer-188c9940.js";import"./model-d2e70025.js";const G={title:"Json Schema Diff Viewer",component:M,parameters:{},argTypes:{},args:{schema:{}}},r={args:{schema:i({beforeSchema:{},afterSchema:{},target:s}),layoutMode:n,metaKeys:{diffsMetaKey:e,aggregatedDiffsMetaKey:t}}},a={args:{schema:{type:"object",properties:{newProperty:{type:"string"},anotherProperty:{type:"string"},[e]:{newProperty:{beforeKey:"oldProperty",afterKey:"newProperty",type:"annotation",action:"rename"}}}},layoutMode:n,metaKeys:{diffsMetaKey:e,aggregatedDiffsMetaKey:t}}},d={args:{schema:i({beforeSchema:{type:"object",required:["madeOptional","unchangedRequired","removedRequired"],properties:{madeRequired:{type:"string"},madeOptional:{type:"string"},madeReadOnly:{type:"integer"},madeWriteOnly:{type:"integer"},madeDeprecated:{type:"integer"},madeWritable:{type:"boolean",readOnly:!0},madeReadable:{type:"boolean",writeOnly:!0},madeActual:{type:"boolean",deprecated:!0},removedReadOnly:{type:"number",readOnly:!0},removedWriteOnly:{type:"number",writeOnly:!0},removedRequired:{type:"number"},removed:{type:"number"},unchangedRequired:{type:"string"},unchangedOptional:{type:"string"},unchangedReadOnly:{type:"string",readOnly:!0},unchangedWriteOnly:{type:"string",writeOnly:!0},unchangedDeprecated:{type:"string",deprecated:!0}}},afterSchema:{type:"object",required:["madeRequired","unchangedRequired","addedRequired"],properties:{madeRequired:{type:"string"},madeOptional:{type:"string"},madeReadOnly:{type:"integer",readOnly:!0},madeWriteOnly:{type:"integer",writeOnly:!0},madeDeprecated:{type:"integer",deprecated:!0},madeWritable:{type:"boolean"},madeReadable:{type:"boolean"},madeActual:{type:"boolean"},addedReadOnly:{type:"number",readOnly:!0},addedWriteOnly:{type:"number",writeOnly:!0},addedRequired:{type:"number"},added:{type:"number"},unchangedRequired:{type:"string"},unchangedOptional:{type:"string"},unchangedReadOnly:{type:"string",readOnly:!0},unchangedWriteOnly:{type:"string",writeOnly:!0},unchangedDeprecated:{type:"string",deprecated:!0}}},target:s}),layoutMode:n,metaKeys:{diffsMetaKey:e,aggregatedDiffsMetaKey:t}}},p={args:{schema:i({beforeSchema:{type:"object",properties:{addedEnum:{type:"string"},removedEnum:{type:"string",enum:["value1","value2"]},addedEnumValue:{type:"string",enum:["value1"]},removedEnumValue:{type:"string",enum:["value1","value2"]},unchangedEnumValue:{type:"string",enum:["value1","value2"]}}},afterSchema:{type:"object",properties:{addedEnum:{type:"string",enum:["value1","value2"]},removedEnum:{type:"string"},addedEnumValue:{type:"string",enum:["value1","value2"]},removedEnumValue:{type:"string",enum:["value1"]},unchangedEnumValue:{type:"string",enum:["value1","value2"]}}},target:s}),layoutMode:n,metaKeys:{diffsMetaKey:e,aggregatedDiffsMetaKey:t}}},y={args:{schema:i({beforeSchema:{type:"object",properties:{option1:{type:"array",minItems:0,items:{type:"string"}},option2:{type:"array",items:{type:"string"}}}},afterSchema:{type:"object",properties:{option1:{type:"array",minItems:1,items:{type:"string"}},option2:{type:"array",minItems:1,items:{type:"string"}}}},target:s}),layoutMode:n,metaKeys:{diffsMetaKey:e,aggregatedDiffsMetaKey:t}}};var o,m,u;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    schema: prepareJsonDiffSchema({
+      beforeSchema: {},
+      afterSchema: {},
+      target: RESPONSE_200_BODY_TARGET
+    }),
+    layoutMode: SIDE_BY_SIDE_DIFFS_LAYOUT_MODE,
+    metaKeys: {
+      diffsMetaKey: DIFF_META_KEY,
+      aggregatedDiffsMetaKey: DIFFS_AGGREGATED_META_KEY
+    }
+  }
+}`,...(u=(m=r.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var g,l,c;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    schema: {
+      type: 'object',
+      properties: {
+        newProperty: {
+          type: 'string'
+        },
+        anotherProperty: {
+          type: 'string'
+        },
+        [DIFF_META_KEY]: {
+          newProperty: {
+            beforeKey: 'oldProperty',
+            afterKey: 'newProperty',
+            type: 'annotation',
+            action: 'rename'
+          }
+        }
+      }
+    },
+    layoutMode: SIDE_BY_SIDE_DIFFS_LAYOUT_MODE,
+    metaKeys: {
+      diffsMetaKey: DIFF_META_KEY,
+      aggregatedDiffsMetaKey: DIFFS_AGGREGATED_META_KEY
+    }
+  }
+}`,...(c=(l=a.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var E,O,_;d.parameters={...d.parameters,docs:{...(E=d.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    schema: prepareJsonDiffSchema({
+      beforeSchema: {
+        type: 'object',
+        required: ['madeOptional', 'unchangedRequired', 'removedRequired'],
+        properties: {
+          madeRequired: {
+            type: 'string'
+          },
+          madeOptional: {
+            type: 'string'
+          },
+          madeReadOnly: {
+            type: 'integer'
+          },
+          madeWriteOnly: {
+            type: 'integer'
+          },
+          madeDeprecated: {
+            type: 'integer'
+          },
+          madeWritable: {
+            type: 'boolean',
+            readOnly: true
+          },
+          madeReadable: {
+            type: 'boolean',
+            writeOnly: true
+          },
+          madeActual: {
+            type: 'boolean',
+            deprecated: true
+          },
+          removedReadOnly: {
+            type: 'number',
+            readOnly: true
+          },
+          removedWriteOnly: {
+            type: 'number',
+            writeOnly: true
+          },
+          removedRequired: {
+            type: 'number'
+          },
+          removed: {
+            type: 'number'
+          },
+          unchangedRequired: {
+            type: 'string'
+          },
+          unchangedOptional: {
+            type: 'string'
+          },
+          unchangedReadOnly: {
+            type: 'string',
+            readOnly: true
+          },
+          unchangedWriteOnly: {
+            type: 'string',
+            writeOnly: true
+          },
+          unchangedDeprecated: {
+            type: 'string',
+            deprecated: true
+          }
+        }
+      },
+      afterSchema: {
+        type: 'object',
+        required: ['madeRequired', 'unchangedRequired', 'addedRequired'],
+        properties: {
+          madeRequired: {
+            type: 'string'
+          },
+          madeOptional: {
+            type: 'string'
+          },
+          madeReadOnly: {
+            type: 'integer',
+            readOnly: true
+          },
+          madeWriteOnly: {
+            type: 'integer',
+            writeOnly: true
+          },
+          madeDeprecated: {
+            type: 'integer',
+            deprecated: true
+          },
+          madeWritable: {
+            type: 'boolean'
+          },
+          madeReadable: {
+            type: 'boolean'
+          },
+          madeActual: {
+            type: 'boolean'
+          },
+          addedReadOnly: {
+            type: 'number',
+            readOnly: true
+          },
+          addedWriteOnly: {
+            type: 'number',
+            writeOnly: true
+          },
+          addedRequired: {
+            type: 'number'
+          },
+          added: {
+            type: 'number'
+          },
+          unchangedRequired: {
+            type: 'string'
+          },
+          unchangedOptional: {
+            type: 'string'
+          },
+          unchangedReadOnly: {
+            type: 'string',
+            readOnly: true
+          },
+          unchangedWriteOnly: {
+            type: 'string',
+            writeOnly: true
+          },
+          unchangedDeprecated: {
+            type: 'string',
+            deprecated: true
+          }
+        }
+      },
+      target: RESPONSE_200_BODY_TARGET
+    }),
+    layoutMode: SIDE_BY_SIDE_DIFFS_LAYOUT_MODE,
+    metaKeys: {
+      diffsMetaKey: DIFF_META_KEY,
+      aggregatedDiffsMetaKey: DIFFS_AGGREGATED_META_KEY
+    }
+  }
+}`,...(_=(O=d.parameters)==null?void 0:O.docs)==null?void 0:_.source}}};var f,D,h;p.parameters={...p.parameters,docs:{...(f=p.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    schema: prepareJsonDiffSchema({
+      beforeSchema: {
+        type: 'object',
+        properties: {
+          addedEnum: {
+            type: 'string'
+          },
+          removedEnum: {
+            type: 'string',
+            enum: ['value1', 'value2']
+          },
+          addedEnumValue: {
+            type: 'string',
+            enum: ['value1']
+          },
+          removedEnumValue: {
+            type: 'string',
+            enum: ['value1', 'value2']
+          },
+          unchangedEnumValue: {
+            type: 'string',
+            enum: ['value1', 'value2']
+          }
+        }
+      },
+      afterSchema: {
+        type: 'object',
+        properties: {
+          addedEnum: {
+            type: 'string',
+            enum: ['value1', 'value2']
+          },
+          removedEnum: {
+            type: 'string'
+          },
+          addedEnumValue: {
+            type: 'string',
+            enum: ['value1', 'value2']
+          },
+          removedEnumValue: {
+            type: 'string',
+            enum: ['value1']
+          },
+          unchangedEnumValue: {
+            type: 'string',
+            enum: ['value1', 'value2']
+          }
+        }
+      },
+      target: RESPONSE_200_BODY_TARGET
+    }),
+    layoutMode: SIDE_BY_SIDE_DIFFS_LAYOUT_MODE,
+    metaKeys: {
+      diffsMetaKey: DIFF_META_KEY,
+      aggregatedDiffsMetaKey: DIFFS_AGGREGATED_META_KEY
+    }
+  }
+}`,...(h=(D=p.parameters)==null?void 0:D.docs)==null?void 0:h.source}}};var S,b,R;y.parameters={...y.parameters,docs:{...(S=y.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    schema: prepareJsonDiffSchema({
+      beforeSchema: {
+        type: 'object',
+        properties: {
+          option1: {
+            type: 'array',
+            minItems: 0,
+            items: {
+              type: 'string'
+            }
+          },
+          option2: {
+            type: 'array',
+            items: {
+              type: 'string'
+            }
+          }
+        }
+      },
+      afterSchema: {
+        type: 'object',
+        properties: {
+          option1: {
+            type: 'array',
+            minItems: 1,
+            items: {
+              type: 'string'
+            }
+          },
+          option2: {
+            type: 'array',
+            minItems: 1,
+            items: {
+              type: 'string'
+            }
+          }
+        }
+      },
+      target: RESPONSE_200_BODY_TARGET
+    }),
+    layoutMode: SIDE_BY_SIDE_DIFFS_LAYOUT_MODE,
+    metaKeys: {
+      diffsMetaKey: DIFF_META_KEY,
+      aggregatedDiffsMetaKey: DIFFS_AGGREGATED_META_KEY
+    }
+  }
+}`,...(R=(b=y.parameters)==null?void 0:b.docs)==null?void 0:R.source}}};const w=["Test","RenamedProperty","Flags","Enum","AddMinItemsInArrayProperty"];export{y as AddMinItemsInArrayProperty,p as Enum,d as Flags,a as RenamedProperty,r as Test,w as __namedExportsOrder,G as default};
---
+++ feature/lazy-tree-building/assets/graph-api-transformers-08e791de.js
@@ -0,0 +1,1 @@
+import{b as r}from"./ErrorBoundaryFallback-1f8fbe38.js";import{b as a}from"./buildASTSchema-f9c3f9c5.js";function t(o){return r(a(o,{noLocation:!0}))}export{t as b};
