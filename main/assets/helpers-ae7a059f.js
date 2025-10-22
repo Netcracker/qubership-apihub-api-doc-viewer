5d1cde974b6a662c1c13fd8d79fb9c2c20e66682
b41ex <b41ex@users.noreply.github.com>
2025-10-22 15:28:03 +0000

    deploy: f91fa691f01f121a5d55f008babf14e1ebd12e65

---
+++ main/assets/helpers-ae7a059f.js
@@ -0,0 +1,1 @@
+import{b as o}from"./ErrorBoundaryFallback-3ed09689.js";import{b as i}from"./buildASTSchema-4610b028.js";function a(r){return o(i(r,{noLocation:!0}))}function u(r){return a(r[0])}export{u as g};
