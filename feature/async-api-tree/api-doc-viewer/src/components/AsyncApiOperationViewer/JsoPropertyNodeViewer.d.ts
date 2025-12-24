f1fbc4b645a818b52fe662251ae27fc18011da73
JayLim2 <JayLim2@users.noreply.github.com>
2025-12-24 14:42:43 +0000

    deploy: d99c78f4b2373607e48cb07727bf60c261089010

--- feature/async-api-tree/api-doc-viewer/src/components/AsyncApiOperationViewer/JsoPropertyNodeViewer.d.ts
+++ feature/async-api-tree/api-doc-viewer/src/components/AsyncApiOperationViewer/JsoPropertyNodeViewer.d.ts
@@ -3,6 +3,9 @@ import { AsyncApiTreeNodeKinds } from '../../../../next-data-model/src/model/asy
 import { FC } from '../../../../../node_modules/react';
 type JsoPropertyNodeViewerProps = {
     node: AsyncApiTreeNode<typeof AsyncApiTreeNodeKinds.JSO_PROPERTY>;
+    expandable: boolean;
+    expanded?: boolean;
+    level: number;
 };
 export declare const JsoPropertyNodeViewer: FC<JsoPropertyNodeViewerProps>;
 export {};
---
+++ feature/async-api-tree/api-doc-viewer/src/utils/async-api/node-sorting-by-display-priority.d.ts
@@ -0,0 +1,3 @@
+import { AsyncApiTreeNode } from '../../../../next-data-model/src/model/async-api/types/aliases';
+import { AsyncApiTreeNodeKind } from '../../../../next-data-model/src/model/async-api/types/node-kind';
+export declare function sortNodesByDisplayPriority(sortFor: AsyncApiTreeNodeKind, nodes: AsyncApiTreeNode[]): AsyncApiTreeNode[];
