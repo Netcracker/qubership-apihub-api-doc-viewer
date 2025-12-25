39a40402019062d3ac15fe2edeba086d214d0341
JayLim2 <JayLim2@users.noreply.github.com>
2025-12-25 11:31:37 +0000

    deploy: 0e2ce66f11947f65cf3a626dc0b6519e55a98cbf

--- feature/async-api-tree/api-doc-viewer/src/components/AsyncApiOperationViewer/OperationNodeViewer.d.ts
+++ feature/async-api-tree/api-doc-viewer/src/components/AsyncApiOperationViewer/OperationNodeViewer.d.ts
@@ -1,10 +1,8 @@
-import { ITreeNode } from '../../../../next-data-model/src/model/abstract/tree/tree-node.interface';
+import { AsyncApiTreeNode } from '../../../../next-data-model/src/model/async-api/types/aliases';
 import { AsyncApiTreeNodeKinds } from '../../../../next-data-model/src/model/async-api/types/node-kind';
-import { AsyncApiNodeMeta } from '../../../../next-data-model/src/model/async-api/types/node-meta';
-import { AsyncApiTreeNodeValue } from '../../../../next-data-model/src/model/async-api/types/node-value';
 import { FC } from '../../../../../node_modules/react';
 type OperationNodeViewerProps = {
-    node: ITreeNode<AsyncApiTreeNodeValue<typeof AsyncApiTreeNodeKinds.OPERATION> | null, typeof AsyncApiTreeNodeKinds.OPERATION, AsyncApiNodeMeta>;
+    node: AsyncApiTreeNode<typeof AsyncApiTreeNodeKinds.OPERATION>;
 };
 export declare const OperationNodeViewer: FC<OperationNodeViewerProps>;
 export {};
