f7e057dbdd8e8ba7fcdc9904c0cc21da2b1b6332
vOrigins <vOrigins@users.noreply.github.com>
2026-01-13 12:38:00 +0000

    deploy: 61ffa994b2ea895bb890c35630d9ed64591d931c

---
+++ feature/oas-30-31-priority-1-fail/assets/JsonSchemaTest.stories-a9d5c72f.js
@@ -0,0 +1,35 @@
+import{J as m}from"./JsonSchemaViewer-7c7e9951.js";import{c as p,d as i}from"./preprocess-6a722d22.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./ErrorBoundaryFallback-9d599c59.js";import"./index-415bee12.js";import"./JsonPropNodeViewer-f0972416.js";const T={title:"Json Schema Viewer",component:m,parameters:{},argTypes:{},args:{schema:{}}},e={args:{schema:{}}},s={args:{schema:p({schema:{type:"object",properties:{a:{$ref:"#/components/schemas/A"},b:{$ref:"#/components/schemas/A"}}},target:i,additionalComponents:{schemas:{A:{type:"object",properties:{c:{$ref:"#/components/schemas/A"}}}}},circular:!0})}};var r,n,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    schema: {}
+  }
+}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var o,t,c;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    schema: prepareJsonSchema({
+      schema: {
+        type: 'object',
+        properties: {
+          a: {
+            $ref: '#/components/schemas/A'
+          },
+          b: {
+            $ref: '#/components/schemas/A'
+          }
+        }
+      },
+      target: REQUEST_BODY_TARGET,
+      additionalComponents: {
+        schemas: {
+          A: {
+            type: 'object',
+            properties: {
+              c: {
+                $ref: '#/components/schemas/A'
+              }
+            }
+          }
+        }
+      },
+      circular: true
+    })
+  }
+}`,...(c=(t=s.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};const S=["Test","Cycled"];export{s as Cycled,e as Test,S as __namedExportsOrder,T as default};
