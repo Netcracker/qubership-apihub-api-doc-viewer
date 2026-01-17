dfe715cd0f107449a27f54ec85b75663b4895efe
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-01-17 17:53:07 +0400

    chore: Add stories in storybook to cover cases of spec extensions in JSON Schema Viewer

--- packages/api-doc-viewer/src/stories/JsonSchemaTest.stories.tsx
+++ packages/api-doc-viewer/src/stories/JsonSchemaTest.stories.tsx
@@ -75,7 +75,7 @@ export const Cycled: Story = {
   }
 }
 
-export const Extensions: Story = {
+export const ExtensionsFirstLevel: Story = {
   args: {
     schema: prepareJsonSchema({
       target: REQUEST_BODY_TARGET,
@@ -90,7 +90,7 @@ export const Extensions: Story = {
   }
 }
 
-export const ExtensionsDeeper: Story = {
+export const ExtensionsSecondLevel: Story = {
   args: {
     schema: prepareJsonSchema({
       target: REQUEST_BODY_TARGET,
@@ -104,6 +104,83 @@ export const ExtensionsDeeper: Story = {
             'x-min-date': '2026-01-01',
             'x-max-date': '2026-12-31',
           }
+        }
+      }
+    })
+  }
+}
+
+export const MultiLevelExtensionsFirstLevel: Story = {
+  args: {
+    schema: prepareJsonSchema({
+      target: REQUEST_BODY_TARGET,
+      schema: {
+        type: 'string',
+        format: 'date-time',
+        description: 'Date Time in Range',
+        'x-date': {
+          min: '2026-01-01',
+          max: '2026-12-31',
+        },
+        'x-date-time': {
+          min: '2026-01-01T00:00:00Z',
+          max: '2026-12-31T23:59:59Z',
+        },
+      }
+    })
+  }
+}
+
+export const MultiLevelExtensionsSecondLevel: Story = {
+  args: {
+    schema: prepareJsonSchema({
+      target: REQUEST_BODY_TARGET,
+      schema: {
+        type: 'object',
+        properties: {
+          a: {
+            type: 'string',
+            format: 'date-time',
+            description: 'Date Time in Range',
+            'x-date': {
+              min: '2026-01-01',
+              max: '2026-12-31',
+            },
+            'x-date-time': {
+              min: '2026-01-01T00:00:00Z',
+              max: '2026-12-31T23:59:59Z',
+            },
+          }
+        }
+      }
+    })
+  }
+}
+
+export const CombinationOfDifferentExtensionsSecondLevel: Story = {
+  args: {
+    schema: prepareJsonSchema({
+      target: REQUEST_BODY_TARGET,
+      schema: {
+        type: 'array',
+        'x-sort-order': 'asc',
+        'x-sort-keys': ['date', 'time'],
+        items: {
+          type: 'string',
+          format: 'date-time',
+          description: 'Date Time in Range',
+          'x-default-timezone': 'UTC',
+          'x-allowed-timezones': ['UTC', 'America/New_York', 'Europe/Paris', 'Asia/Tokyo'],
+          'x-range': {
+            min: {
+              date: '2026-01-01',
+              time: '00:00:00Z',
+            },
+            max: {
+              date: '2026-12-31',
+              time: '23:59:59Z',
+            },
+          },
         }
       }
     })
