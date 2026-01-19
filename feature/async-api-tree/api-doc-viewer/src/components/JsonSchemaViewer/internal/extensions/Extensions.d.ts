e2f10f1d820b00f1eaa669820023ab96e8db2819
JayLim2 <JayLim2@users.noreply.github.com>
2026-01-19 22:45:51 +0000

    deploy: 41fa292cb63ff43fef34f3c2f1c5d510ef67faba

--- feature/async-api-tree/api-doc-viewer/src/components/JsonSchemaViewer/internal/extensions/Extensions.d.ts
+++ feature/async-api-tree/api-doc-viewer/src/components/JsonSchemaViewer/internal/extensions/Extensions.d.ts
@@ -2,7 +2,7 @@ import { IJsonSchemaBaseType } from '../../../../../../api-data-model/src';
 import { FC } from '../../../../../../../node_modules/react';
 type ExtensionsProps = {
     shift?: boolean;
-    extensions: IJsonSchemaBaseType['extensions'];
+    extensions: NonNullable<IJsonSchemaBaseType['extensions']>;
 };
 export declare const Extensions: FC<ExtensionsProps>;
 export {};
--- feature/async-api-tree/assets/graph-api-transformers-9fb2d9f5.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./GraphPropNodeViewer-e7edf85d.js";import{b as a}from"./buildASTSchema-f14864f0.js";function t(o){return r(a(o,{noLocation:!0}))}export{t as b};
