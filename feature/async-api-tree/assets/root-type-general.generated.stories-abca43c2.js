1828e64b802c6aea2c901f3c4e25f2aa37db6b2c
JayLim2 <JayLim2@users.noreply.github.com>
2026-01-26 14:30:20 +0000

    deploy: d42f853df40658d206964bf439ee5014c837d746

---
+++ feature/async-api-tree/assets/root-type-general.generated.stories-abca43c2.js
@@ -0,0 +1,21 @@
+import"./AsyncApiOperationViewer-d82fe121.js";import"./GraphQLOperationViewer-048505bc.js";import"./GraphQLOperationDiffViewer-bd7f1ad0.js";import"./GraphPropNodeViewer-9a3dca89.js";import{G as e,g as r,T as o}from"./compatibility-suite-utils-b680899a.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./index-415bee12.js";import"./graph-api-transformers-27c02a61.js";import"./buildASTSchema-f14864f0.js";const Y={title:"graphql-compatibility-suite/root-type-general"},t={name:"add-description-for-root-type",render:e,args:r(o,"root-type-general","add-description-for-root-type")},a={name:"add-new-root-type",render:e,args:r(o,"root-type-general","add-new-root-type")},p={name:"change-description-for-root-type",render:e,args:r(o,"root-type-general","change-description-for-root-type")},n={name:"delete-description-for-root-type",render:e,args:r(o,"root-type-general","delete-description-for-root-type")},s={name:"delete-root-type",render:e,args:r(o,"root-type-general","delete-root-type")};var d,i,c;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: 'add-description-for-root-type',
+  render: GraphQLStoryComponent,
+  args: getStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, 'root-type-general', 'add-description-for-root-type')
+}`,...(c=(i=t.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var y,m,g;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  name: 'add-new-root-type',
+  render: GraphQLStoryComponent,
+  args: getStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, 'root-type-general', 'add-new-root-type')
+}`,...(g=(m=a.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var l,S,T;p.parameters={...p.parameters,docs:{...(l=p.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: 'change-description-for-root-type',
+  render: GraphQLStoryComponent,
+  args: getStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, 'root-type-general', 'change-description-for-root-type')
+}`,...(T=(S=p.parameters)==null?void 0:S.docs)==null?void 0:T.source}}};var _,E,P;n.parameters={...n.parameters,docs:{...(_=n.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  name: 'delete-description-for-root-type',
+  render: GraphQLStoryComponent,
+  args: getStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, 'root-type-general', 'delete-description-for-root-type')
+}`,...(P=(E=n.parameters)==null?void 0:E.docs)==null?void 0:P.source}}};var u,A,R;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  name: 'delete-root-type',
+  render: GraphQLStoryComponent,
+  args: getStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, 'root-type-general', 'delete-root-type')
+}`,...(R=(A=s.parameters)==null?void 0:A.docs)==null?void 0:R.source}}};const x=["AddDescriptionForRootType","AddNewRootType","ChangeDescriptionForRootType","DeleteDescriptionForRootType","DeleteRootType"];export{t as AddDescriptionForRootType,a as AddNewRootType,p as ChangeDescriptionForRootType,n as DeleteDescriptionForRootType,s as DeleteRootType,x as __namedExportsOrder,Y as default};
