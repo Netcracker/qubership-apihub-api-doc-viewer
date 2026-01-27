9b04fe110287f5c6d31aec1bc08c4c7de39aec73
JayLim2 <JayLim2@users.noreply.github.com>
2026-01-27 09:35:39 +0000

    deploy: efdb1f764eeffa2938d061f3f079d3b8f0ba0383

--- feature/async-api-tree/api-doc-viewer/src/components/AsyncApiOperationViewer/AsyncApiOperationViewer.d.ts
+++ feature/async-api-tree/api-doc-viewer/src/components/AsyncApiOperationViewer/AsyncApiOperationViewer.d.ts
@@ -1,9 +1,11 @@
 import { DisplayMode } from '../../types/DisplayMode';
 import { FC } from '../../../../../node_modules/react';
+import { ReferenceNameMapping } from '../../../../next-data-model/src/model/async-api/types/reference-name-property-mapping';
 export type AsyncApiOperationViewerProps = {
     source: unknown;
     operationType?: string;
     operationName?: string;
     displayMode?: DisplayMode;
+    referenceNamePropertyMapping?: ReferenceNameMapping;
 };
 export declare const AsyncApiOperationViewer: FC<AsyncApiOperationViewerProps>;
