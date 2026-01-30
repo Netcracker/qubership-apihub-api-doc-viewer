2fb8317e56e7d66943b53c5fb7d4464a19c11855
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-01-31 01:08:50 +0400

    fix: Unlocked rendering

--- packages/api-doc-viewer/src/components/AsyncApiOperationViewer/AsyncApiOperationViewer.tsx
+++ packages/api-doc-viewer/src/components/AsyncApiOperationViewer/AsyncApiOperationViewer.tsx
@@ -59,10 +59,6 @@ const AsyncApiOperationViewerInner: FC<AsyncApiOperationViewerProps> =
       return null
     }
 
-    if (Math.random() > -1) {
-      return null
-    }
-
     return (
       <DisplayModeContext.Provider value={displayMode}>
         <LayoutModeContext.Provider value={DOCUMENT_LAYOUT_MODE}> {/* Now only 1 layout mode is supported */}
