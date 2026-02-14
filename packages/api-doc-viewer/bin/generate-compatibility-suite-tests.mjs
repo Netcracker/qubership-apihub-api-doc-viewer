c564842423b5db25187faa5173a510675b30cfbf
Vladyslav Novikov <vladyslav.novikov@netcracker.com>
2026-02-14 15:07:55 +0000

    test(schema-reuse): temporarily disable asyncapi test generation

--- packages/api-doc-viewer/bin/generate-compatibility-suite-tests.mjs
+++ packages/api-doc-viewer/bin/generate-compatibility-suite-tests.mjs
@@ -26,7 +26,16 @@ const TEST_GENERATION_CONFIGS = [
   },
   {
     specType: TEST_SPEC_TYPE_ASYNC_API,
-    // skipSuiteIds: new Set(['operation-reply-object-message-headers-receive']),
+    skipSuiteIds: new Set([
+      'operation-message-headers-receive',
+      'operation-message-headers-send',
+      'operation-message-payload-receive',
+      'operation-message-payload-send',
+      'operation-reply-object-message-headers-receive',
+      'operation-reply-object-message-headers-send',
+      'operation-reply-object-message-payload-receive',
+      'operation-reply-object-message-payload-send',
+    ]),
   },
 ]
 
