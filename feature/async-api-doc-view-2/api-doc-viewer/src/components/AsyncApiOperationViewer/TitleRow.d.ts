9c7274a01160c221d822c2ecbf66cc62d6c637a0
JayLim2 <JayLim2@users.noreply.github.com>
2026-03-04 08:18:41 +0000

    deploy: ab6706ee347e13ce9f73906f0a649d7d89aff18a

--- feature/async-api-doc-view-2/api-doc-viewer/src/components/AsyncApiOperationViewer/TitleRow.d.ts
+++ feature/async-api-doc-view-2/api-doc-viewer/src/components/AsyncApiOperationViewer/TitleRow.d.ts
@@ -6,6 +6,8 @@ export declare const TitleVariant: {
     readonly h2: "h2";
     readonly h3: "h3";
     readonly h4: "h4";
+    readonly h5: "h5";
+    readonly h6: "h6";
     readonly body: "body";
 };
 export type TitleVariant = typeof TitleVariant[keyof typeof TitleVariant];
