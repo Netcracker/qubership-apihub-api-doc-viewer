1e7108601576600b44de9e857a5a8594541cac58
JayLim2 <JayLim2@users.noreply.github.com>
2026-03-06 14:25:39 +0000

    deploy: cf8e69528bbf0c6e60ee3f8b1ccda13be0332dc8

--- feature/async-api-doc-view-2/api-doc-viewer/src/components/AsyncApiOperationViewer/AsyncApiOperationViewer.d.ts
+++ feature/async-api-doc-view-2/api-doc-viewer/src/components/AsyncApiOperationViewer/AsyncApiOperationViewer.d.ts
@@ -2,7 +2,6 @@ import { DisplayMode } from '../../types/DisplayMode';
 import { FC } from '../../../../../node_modules/react';
 export type AsyncApiOperationViewerProps = {
     source: unknown;
-    operationType?: string;
     operationKey?: string;
     messageKey?: string;
     displayMode?: DisplayMode;
