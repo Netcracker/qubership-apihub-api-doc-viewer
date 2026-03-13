87ad28f0c365088d6b06302fb3dc549f1d8ab140
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-03-13 12:53:54 +0400

    feat: Basic initializing of the branch

---
+++ packages/api-doc-viewer/src/components/AsyncApiOperationViewer/AsyncApiOperationDiffsViewer.tsx
@@ -0,0 +1,83 @@
+import { DEFAULT_DISPLAY_MODE } from "@apihub/constants/configuration";
+import { DisplayModeContext } from "@apihub/contexts/DisplayModeContext";
+import { LayoutModeContext } from "@apihub/contexts/LayoutModeContext";
+import { LevelContext } from "@apihub/contexts/LevelContext";
+import { DisplayMode } from "@apihub/types/DisplayMode";
+import { DOCUMENT_LAYOUT_MODE } from "@apihub/types/LayoutMode";
+import { AsyncApiTreeBuilder, createAsyncApiLogger } from "@netcracker/qubership-apihub-next-data-model";
+import { FC, memo, useMemo } from "react";
+import { ErrorBoundary } from "../services/ErrorBoundary";
+import { ErrorBoundaryFallback } from "../services/ErrorBoundaryFallback";
+import { AsyncApiDevModeContext } from "./AsyncApiDevModeContext";
+import { MessageNodeViewer } from "./MessageNodeViewer";
+
+import { OperationKeys } from "@apihub/next-data-model/shared/async-api/types/operation-keys";
+import { isMessageNode } from "@apihub/utils/async-api/node-type-checkers";
+import '../../index.css';
+import { DiffMetaKeys } from "./types/DiffMetaKeys";
+import { DiffType } from "@netcracker/qubership-apihub-api-diff";
+
+export type AsyncApiOperationDiffsViewerProps = {
+  mergedSource: unknown
+  operationKeys?: OperationKeys
+  displayMode?: DisplayMode
+  devMode?: boolean
+  referenceNamePropertyKey: symbol
+  // diffs specific
+  diffMetaKeys?: DiffMetaKeys
+  diffTypes?: ReadonlyArray<DiffType>
+}
+
+export const AsyncApiOperationDiffsViewer: FC<AsyncApiOperationDiffsViewerProps> =
+  memo<AsyncApiOperationDiffsViewerProps>(props => {
+    if (props.mergedSource === null) {
+      return null
+    }
+
+    return (
+      <ErrorBoundary fallback={<ErrorBoundaryFallback componentName="Async API Operation Viewer" />}>
+        <AsyncApiOperationDiffsViewerInner {...props} />
+      </ErrorBoundary>
+    )
+  })
+
+const AsyncApiOperationDiffsViewerInner: FC<AsyncApiOperationDiffsViewerProps> =
+  memo<AsyncApiOperationDiffsViewerProps>(props => {
+    const {
+      mergedSource: source,
+      operationKeys,
+      displayMode = DEFAULT_DISPLAY_MODE,
+      devMode = false,
+      referenceNamePropertyKey,
+    } = props
+
+    const logger = useMemo(() => createAsyncApiLogger(devMode), [devMode])
+
+    const treeBuilder = useMemo(
+      () => new AsyncApiTreeBuilder(source, referenceNamePropertyKey, operationKeys, logger),
+      [source, operationKeys, referenceNamePropertyKey, logger]
+    )
+    const tree = useMemo(() => treeBuilder?.build() ?? null, [treeBuilder])
+
+    logger.debug('[AsyncAPI Diffs] Original Source:', source)
+    logger.debug('[AsyncAPI Diffs] Tree:', tree)
+
+    const messageNode = tree?.root
+    if (!messageNode || !isMessageNode(messageNode)) {
+      return null
+    }
+
+    return (
+      <AsyncApiDevModeContext.Provider value={devMode}>
+        <DisplayModeContext.Provider value={displayMode}>
+          <LayoutModeContext.Provider value={DOCUMENT_LAYOUT_MODE}> {/* Now only 1 layout mode is supported */}
+            <LevelContext.Provider value={0}>
+              <MessageNodeViewer
+                node={messageNode}
+              />
+            </LevelContext.Provider>
+          </LayoutModeContext.Provider>
+        </DisplayModeContext.Provider>
+      </AsyncApiDevModeContext.Provider>
+    )
+  })
