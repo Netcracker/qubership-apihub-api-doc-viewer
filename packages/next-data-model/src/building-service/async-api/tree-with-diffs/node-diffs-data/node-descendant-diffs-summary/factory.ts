fa4cf4971768defd8543971fe2e695913048fbb0
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-04-01 10:11:49 +0400

    feat: Prepared data model for summary in message sections

--- packages/next-data-model/src/building-service/async-api/tree-with-diffs/node-diffs-data/node-descendant-diffs-summary/factory.ts
+++ packages/next-data-model/src/building-service/async-api/tree-with-diffs/node-diffs-data/node-descendant-diffs-summary/factory.ts
@@ -2,6 +2,9 @@ import { AbstractNodeDescendantsDiffsSummaryAggregator } from "@apihub/next-data
 import { AsyncApiTreeNodeKind, AsyncApiTreeNodeKinds } from "@apihub/next-data-model/model/async-api/types/node-kind";
 import { AsyncApiNodeDescendantDiffsSummaryAggregatorKindAny } from "./kind-any";
 import { AsyncApiNodeDescendantDiffsSummaryAggregatorKindBinding } from "./kind-binding";
+import { AsyncApiNodeDescendantDiffsSummaryAggregatorKindChannel } from "./kind-message-channel";
+import { AsyncApiNodeDescendantDiffsSummaryAggregatorKindContent } from "./kind-message-content";
+import { AsyncApiNodeDescendantDiffsSummaryAggregatorKindOperation } from "./kind-message-operation";
 
 export class AsyncApiNodeDescendantDiffsAggregatorFactory {
   private static readonly instances = new Map<AsyncApiTreeNodeKind | null, AbstractNodeDescendantsDiffsSummaryAggregator>();
@@ -10,6 +13,21 @@ export class AsyncApiNodeDescendantDiffsAggregatorFactory {
     kind: AsyncApiTreeNodeKind,
   ): AbstractNodeDescendantsDiffsSummaryAggregator {
     switch (kind) {
+      case AsyncApiTreeNodeKinds.MESSAGE_CONTENT:
+        if (!this.instances.has(AsyncApiTreeNodeKinds.MESSAGE_CONTENT)) {
+          this.instances.set(AsyncApiTreeNodeKinds.MESSAGE_CONTENT, new AsyncApiNodeDescendantDiffsSummaryAggregatorKindContent());
+        }
+        return this.instances.get(AsyncApiTreeNodeKinds.MESSAGE_CONTENT)!;
+      case AsyncApiTreeNodeKinds.MESSAGE_CHANNEL:
+        if (!this.instances.has(AsyncApiTreeNodeKinds.MESSAGE_CHANNEL)) {
+          this.instances.set(AsyncApiTreeNodeKinds.MESSAGE_CHANNEL, new AsyncApiNodeDescendantDiffsSummaryAggregatorKindChannel());
+        }
+        return this.instances.get(AsyncApiTreeNodeKinds.MESSAGE_CHANNEL)!;
+      case AsyncApiTreeNodeKinds.MESSAGE_OPERATION:
+        if (!this.instances.has(AsyncApiTreeNodeKinds.MESSAGE_OPERATION)) {
+          this.instances.set(AsyncApiTreeNodeKinds.MESSAGE_OPERATION, new AsyncApiNodeDescendantDiffsSummaryAggregatorKindOperation());
+        }
+        return this.instances.get(AsyncApiTreeNodeKinds.MESSAGE_OPERATION)!;
       case AsyncApiTreeNodeKinds.BINDING:
         if (!this.instances.has(AsyncApiTreeNodeKinds.BINDING)) {
           this.instances.set(AsyncApiTreeNodeKinds.BINDING, new AsyncApiNodeDescendantDiffsSummaryAggregatorKindBinding());
