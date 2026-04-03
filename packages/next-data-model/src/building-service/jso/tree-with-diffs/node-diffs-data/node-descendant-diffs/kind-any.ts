648fcb7c576110cdc3f2d9504e03309241e13acc
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-04-03 13:12:36 +0400

    feat: Created diffs aggregators for JsoTreeWithDiffs

---
+++ packages/next-data-model/src/building-service/jso/tree-with-diffs/node-diffs-data/node-descendant-diffs/kind-any.ts
@@ -0,0 +1,16 @@
+import { AbstractNodeDescendantsDiffsAggregator } from "@apihub/next-data-model/building-service/abstract/tree-with-diffs/node-diffs-data/node-descendants-diffs-aggregator";
+import { NodeDescendantDiffs } from "@apihub/next-data-model/model/abstract/tree-with-diffs/tree-node.interface";
+import { DiffMetaKeys } from "../node-diffs/factory";
+
+export class JsoNodeDescendantDiffsAggregatorKindAny extends AbstractNodeDescendantsDiffsAggregator {
+  public aggregate(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    value: object | null,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    diffsMetaKeys: DiffMetaKeys,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    referenceNamePropertyKey: symbol,
+  ): NodeDescendantDiffs | undefined {
+    return undefined;
+  }
+}
