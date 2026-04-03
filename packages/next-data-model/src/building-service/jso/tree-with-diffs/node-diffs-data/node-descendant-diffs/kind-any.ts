4de915c1d741ef4a867c06f398921783e3ffed07
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-04-03 14:53:27 +0400

    feat: Node descendant diffs in JsoDiffsViewer

--- packages/next-data-model/src/building-service/jso/tree-with-diffs/node-diffs-data/node-descendant-diffs/kind-any.ts
+++ packages/next-data-model/src/building-service/jso/tree-with-diffs/node-diffs-data/node-descendant-diffs/kind-any.ts
@@ -1,16 +1,47 @@
 import { AbstractNodeDescendantsDiffsAggregator } from "@apihub/next-data-model/building-service/abstract/tree-with-diffs/node-diffs-data/node-descendants-diffs-aggregator";
-import { NodeDescendantDiffs } from "@apihub/next-data-model/model/abstract/tree-with-diffs/tree-node.interface";
+import { DiffStyles, NodeDescendantDiffs } from "@apihub/next-data-model/model/abstract/tree-with-diffs/tree-node.interface";
+import { isObject, takeIfDiffsRecord } from "@apihub/next-data-model/utilities";
 import { DiffMetaKeys } from "../node-diffs/factory";
 
 export class JsoNodeDescendantDiffsAggregatorKindAny extends AbstractNodeDescendantsDiffsAggregator {
+  private readonly DEFAULT_DIFF_STYLES: DiffStyles = {
+    isContentVisible: true,
+  }
+
   public aggregate(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    value: object | null,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    crawlValue: object | null,
     diffsMetaKeys: DiffMetaKeys,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     referenceNamePropertyKey: symbol,
   ): NodeDescendantDiffs | undefined {
-    return undefined;
+    if (!isObject(crawlValue)) {
+      return undefined;
+    }
+    const value = crawlValue.value
+    if (!isObject(value)) {
+      return undefined
+    }
+
+    const { diffsMetaKey } = diffsMetaKeys
+
+    const diffs = takeIfDiffsRecord(value[diffsMetaKey])
+    if (!diffs) {
+      return undefined
+    }
+
+    const nodeDescendantDiffs: NodeDescendantDiffs = {}
+    for (const [key, diff] of Object.entries(diffs)) {
+      if (diff) {
+        nodeDescendantDiffs[key] = {
+          data: diff,
+          styles: {
+            before: this.DEFAULT_DIFF_STYLES,
+            after: this.DEFAULT_DIFF_STYLES,
+          },
+        }
+      }
+    }
+
+    return nodeDescendantDiffs
   }
 }
