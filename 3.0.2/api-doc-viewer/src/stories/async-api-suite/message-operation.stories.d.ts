f0ca0ff00d039562907c6493247e66e5b47d655f
b41ex <b41ex@users.noreply.github.com>
2026-04-09 08:52:44 +0000

    deploy: 0cd5e87ff626143b1a4b94e122a7fd33727e3999

---
+++ 3.0.2/api-doc-viewer/src/stories/async-api-suite/message-operation.stories.d.ts
@@ -0,0 +1,29 @@
+import type { StoryObj } from '@storybook/react';
+declare const meta: {
+    title: string;
+    component: import('../../../../../node_modules/react').FC<import('../../components/AsyncApiOperationViewer/AsyncApiOperationViewer').AsyncApiOperationViewerProps>;
+    parameters: {};
+    argTypes: {
+        source: {
+            control: "object";
+        };
+    };
+    args: {
+        source: {};
+    };
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+export declare const OperationId: Story;
+export declare const Title: Story;
+export declare const Description: Story;
+export declare const Summary: Story;
+export declare const Extensions: Story;
+export declare const BindingsOneOption: Story;
+export declare const BindingsTwoOptionsSelectedFirst: Story;
+export declare const BindingsTwoOptionsSelectedSecond: Story;
+export declare const DescriptionSummary: Story;
+export declare const DescriptionExtensions: Story;
+export declare const DescriptionBindingsOneOption: Story;
+export declare const ExtensionsBindingsOneOption: Story;
+export declare const EdgeCaseBrokenRefBindings: Story;
