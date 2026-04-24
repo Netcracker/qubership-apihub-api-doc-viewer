ea7634f0f1ad9524d688fec0a59ee824322a6846
JayLim2 <JayLim2@users.noreply.github.com>
2026-04-24 12:10:04 +0000

    deploy: 03602664f575e0b3c78ba582a9a322bc28ef8d03

--- feature/async-api-diff-view/assets/channel-parameters-samples.stories-b408b54b.js
+++ feature/async-api-diff-view/assets/channel-parameters-samples.stories-b408b54b.js
@@ -1,4 +1,4 @@
-import{j as d}from"./_commonjs-dynamic-modules-6308e768.js";import{A as R}from"./AsyncApiOperationDiffsViewer-2ed5022a.js";import{b as D,T as F,a as N}from"./preprocess-442fc617.js";import{c as V,p as B}from"./yaml-source-2575e8ef.js";import"./index-f46741a2.js";import"./GraphPropNodeViewer-74f48742.js";import"./index-415bee12.js";import"./AsyncApiOperationViewer-c25577f7.js";import"./GraphQLOperationViewer-a40cc735.js";import"./GraphQLOperationDiffViewer-e155066c.js";import"./js-yaml-71a24e1f.js";const G=`asyncapi: 3.0.0
+import{j as d}from"./_commonjs-dynamic-modules-6308e768.js";import{A as D}from"./AsyncApiOperationDiffsViewer-2ed5022a.js";import{w as F,u as N}from"./index-d5b0668c.js";import{b as B,T as V,a as G}from"./preprocess-442fc617.js";import{c as z,p as H}from"./yaml-source-2575e8ef.js";import"./index-f46741a2.js";import"./GraphPropNodeViewer-74f48742.js";import"./index-415bee12.js";import"./AsyncApiOperationViewer-c25577f7.js";import"./GraphQLOperationViewer-a40cc735.js";import"./GraphQLOperationDiffViewer-e155066c.js";import"./index-442a5f79.js";import"./js-yaml-71a24e1f.js";const J=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -8,105 +8,6 @@ channels:
     messages:
       TestMessage:
         $ref: '#/components/messages/TestMessage'
-operations:
-  sendOperation:
-    action: send
-    channel:
-      $ref: '#/channels/testChannel'
-    messages:
-      - $ref: '#/components/messages/TestMessage'
-components:
-  messages:
-    TestMessage:
-      name: TestMessage
-      payload:
-        type: object
-        properties:
-          id:
-            type: string
-        required:
-          - id
-`,z=`asyncapi: 3.0.0
-info:
-  title: Sample AsyncAPI
-  version: 1.0.0
-channels:
-  testChannel:
-    address: events.{param1}.{param2}
-    messages:
-      TestMessage:
-        $ref: '#/components/messages/TestMessage'
-    parameters:
-      param1:
-        description: first parameter
-        location: $message.payload#/id
-      param2:
-        description: second parameter
-        location: $message.payload#/id
-operations:
-  sendOperation:
-    action: send
-    channel:
-      $ref: '#/channels/testChannel'
-    messages:
-      - $ref: '#/components/messages/TestMessage'
-components:
-  messages:
-    TestMessage:
-      name: TestMessage
-      payload:
-        type: object
-        properties:
-          id:
-            type: string
-        required:
-          - id
-`,H=`asyncapi: 3.0.0
-info:
-  title: Sample AsyncAPI
-  version: 1.0.0
-channels:
-  testChannel:
-    address: events.{param1}
-    messages:
-      TestMessage:
-        $ref: '#/components/messages/TestMessage'
-    parameters:
-      param1:
-        description: first parameter
-operations:
-  sendOperation:
-    action: send
-    channel:
-      $ref: '#/channels/testChannel'
-    messages:
-      - $ref: '#/components/messages/TestMessage'
-components:
-  messages:
-    TestMessage:
-      name: TestMessage
-      payload:
-        type: object
-        properties:
-          id:
-            type: string
-        required:
-          - id
-`,J=`asyncapi: 3.0.0
-info:
-  title: Sample AsyncAPI
-  version: 1.0.0
-channels:
-  testChannel:
-    address: events.{param1}.{param2}
-    messages:
-      TestMessage:
-        $ref: '#/components/messages/TestMessage'
-    parameters:
-      param1:
-        description: first parameter
-      param2:
-        description: second parameter
 operations:
   sendOperation:
     action: send
@@ -138,6 +39,72 @@ channels:
     parameters:
       param1:
         description: first parameter
+        location: $message.payload#/id
+      param2:
+        description: second parameter
+        location: $message.payload#/id
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,Q=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,U=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}.{param2}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
       param2:
         description: second parameter
 operations:
@@ -158,7 +125,40 @@ components:
             type: string
         required:
           - id
-`,Q=`asyncapi: 3.0.0
+`,W=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}.{param2}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+      param2:
+        description: second parameter
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,X=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -197,7 +197,7 @@ components:
             type: string
         required:
           - id
-`,U=`asyncapi: 3.0.0
+`,Z=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -245,7 +245,7 @@ components:
             type: string
         required:
           - id
-`,W=`asyncapi: 3.0.0
+`,ee=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -289,7 +289,7 @@ components:
             type: string
         required:
           - id
-`,X=`asyncapi: 3.0.0
+`,ne=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -324,7 +324,7 @@ components:
             type: string
         required:
           - id
-`,Z=`asyncapi: 3.0.0
+`,se=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -352,7 +352,7 @@ components:
             type: string
         required:
           - id
-`,ee=`asyncapi: 3.0.0
+`,ae=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -385,7 +385,7 @@ components:
             type: string
         required:
           - id
-`,ne=`asyncapi: 3.0.0
+`,re=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -416,7 +416,7 @@ components:
             type: string
         required:
           - id
-`,se=`asyncapi: 3.0.0
+`,te=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -449,7 +449,7 @@ components:
             type: string
         required:
           - id
-`,ae=`asyncapi: 3.0.0
+`,oe=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -497,7 +497,7 @@ components:
             type: string
         required:
           - id
-`,re=`asyncapi: 3.0.0
+`,me=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -536,7 +536,7 @@ components:
             type: string
         required:
           - id
-`,te=`asyncapi: 3.0.0
+`,pe=`asyncapi: 3.0.0
 info:
   title: Sample AsyncAPI
   version: 1.0.0
@@ -580,4 +580,4 @@ components:
             type: string
         required:
           - id
-`,oe=Object.assign({"../../../../samples/async-api-diffs/channel-parameters/1-channel-parameters-two-added/before.yaml":G,"../../../../samples/async-api-diffs/channel-parameters/2-channel-parameters-two-removed/before.yaml":z,"../../../../samples/async-api-diffs/channel-parameters/3-channel-parameters-second-added/before.yaml":H,"../../../../samples/async-api-diffs/channel-parameters/4-channel-parameters-second-removed/before.yaml":J,"../../../../samples/async-api-diffs/channel-parameters/5-channel-parameters-key-renamed/before.yaml":L,"../../../../samples/async-api-diffs/channel-parameters/6-channel-parameters-fields-added/before.yaml":Q,"../../../../samples/async-api-diffs/channel-parameters/7-channel-parameters-fields-removed/before.yaml":U,"../../../../samples/async-api-diffs/channel-parameters/8-channel-parameters-fields-changed/before.yaml":W}),me=Object.assign({"../../../../samples/async-api-diffs/channel-parameters/1-channel-parameters-two-added/after.yaml":X,"../../../../samples/async-api-diffs/channel-parameters/2-channel-parameters-two-removed/after.yaml":Z,"../../../../samples/async-api-diffs/channel-parameters/3-channel-parameters-second-added/after.yaml":ee,"../../../../samples/async-api-diffs/channel-parameters/4-channel-parameters-second-removed/after.yaml":ne,"../../../../samples/async-api-diffs/channel-parameters/5-channel-parameters-key-renamed/after.yaml":se,"../../../../samples/async-api-diffs/channel-parameters/6-channel-parameters-fields-added/after.yaml":ae,"../../../../samples/async-api-diffs/channel-parameters/7-channel-parameters-fields-removed/after.yaml":re,"../../../../samples/async-api-diffs/channel-parameters/8-channel-parameters-fields-changed/after.yaml":te}),pe=V(oe,me),ie=Object.fromEntries(pe.map(e=>[e.caseId,e])),k=({caseId:e})=>{const s=ie[e];return s?d.jsx(R,{...le(s.beforeYaml,s.afterYaml)}):d.jsxs("div",{children:["Sample case not found: ",e]})},Ce={title:"Async API Diffs Suite/Channel Parameters Samples",component:k},ce="sendOperation",de="TestMessage",l=e=>B(e),le=(e,s)=>({devMode:!0,mergedSource:D({beforeSource:l(e),afterSource:l(s)}),operationKeys:{operationKey:ce,messageKey:de},referenceNamePropertyKey:F,diffMetaKeys:N}),n=e=>({name:e,args:{caseId:e},render:s=>d.jsx(k,{caseId:s.caseId})}),a=n("1-channel-parameters-two-added"),r=n("2-channel-parameters-two-removed"),t=n("3-channel-parameters-second-added"),o=n("4-channel-parameters-second-removed"),m=n("5-channel-parameters-key-renamed"),p=n("6-channel-parameters-fields-added"),i=n("7-channel-parameters-fields-removed"),c=n("8-channel-parameters-fields-changed");var g,f,h;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:'createCaseStory("1-channel-parameters-two-added")',...(h=(f=a.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var _,y,T;r.parameters={...r.parameters,docs:{...(_=r.parameters)==null?void 0:_.docs,source:{originalSource:'createCaseStory("2-channel-parameters-two-removed")',...(T=(y=r.parameters)==null?void 0:y.docs)==null?void 0:T.source}}};var M,P,u;t.parameters={...t.parameters,docs:{...(M=t.parameters)==null?void 0:M.docs,source:{originalSource:'createCaseStory("3-channel-parameters-second-added")',...(u=(P=t.parameters)==null?void 0:P.docs)==null?void 0:u.source}}};var v,$,C;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:'createCaseStory("4-channel-parameters-second-removed")',...(C=($=o.parameters)==null?void 0:$.docs)==null?void 0:C.source}}};var b,S,A;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:'createCaseStory("5-channel-parameters-key-renamed")',...(A=(S=m.parameters)==null?void 0:S.docs)==null?void 0:A.source}}};var O,j,I;p.parameters={...p.parameters,docs:{...(O=p.parameters)==null?void 0:O.docs,source:{originalSource:'createCaseStory("6-channel-parameters-fields-added")',...(I=(j=p.parameters)==null?void 0:j.docs)==null?void 0:I.source}}};var x,E,q;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:'createCaseStory("7-channel-parameters-fields-removed")',...(q=(E=i.parameters)==null?void 0:E.docs)==null?void 0:q.source}}};var w,K,Y;c.parameters={...c.parameters,docs:{...(w=c.parameters)==null?void 0:w.docs,source:{originalSource:'createCaseStory("8-channel-parameters-fields-changed")',...(Y=(K=c.parameters)==null?void 0:K.docs)==null?void 0:Y.source}}};const be=["Case_1_channel_parameters_two_added","Case_2_channel_parameters_two_removed","Case_3_channel_parameters_second_added","Case_4_channel_parameters_second_removed","Case_5_channel_parameters_key_renamed","Case_6_channel_parameters_fields_added","Case_7_channel_parameters_fields_removed","Case_8_channel_parameters_fields_changed"];export{a as Case_1_channel_parameters_two_added,r as Case_2_channel_parameters_two_removed,t as Case_3_channel_parameters_second_added,o as Case_4_channel_parameters_second_removed,m as Case_5_channel_parameters_key_renamed,p as Case_6_channel_parameters_fields_added,i as Case_7_channel_parameters_fields_removed,c as Case_8_channel_parameters_fields_changed,be as __namedExportsOrder,Ce as default};
+`,ie=Object.assign({"../../../../samples/async-api-diffs/channel-parameters/1-channel-parameters-two-added/before.yaml":J,"../../../../samples/async-api-diffs/channel-parameters/2-channel-parameters-two-removed/before.yaml":L,"../../../../samples/async-api-diffs/channel-parameters/3-channel-parameters-second-added/before.yaml":Q,"../../../../samples/async-api-diffs/channel-parameters/4-channel-parameters-second-removed/before.yaml":U,"../../../../samples/async-api-diffs/channel-parameters/5-channel-parameters-key-renamed/before.yaml":W,"../../../../samples/async-api-diffs/channel-parameters/6-channel-parameters-fields-added/before.yaml":X,"../../../../samples/async-api-diffs/channel-parameters/7-channel-parameters-fields-removed/before.yaml":Z,"../../../../samples/async-api-diffs/channel-parameters/8-channel-parameters-fields-changed/before.yaml":ee}),ce=Object.assign({"../../../../samples/async-api-diffs/channel-parameters/1-channel-parameters-two-added/after.yaml":ne,"../../../../samples/async-api-diffs/channel-parameters/2-channel-parameters-two-removed/after.yaml":se,"../../../../samples/async-api-diffs/channel-parameters/3-channel-parameters-second-added/after.yaml":ae,"../../../../samples/async-api-diffs/channel-parameters/4-channel-parameters-second-removed/after.yaml":re,"../../../../samples/async-api-diffs/channel-parameters/5-channel-parameters-key-renamed/after.yaml":te,"../../../../samples/async-api-diffs/channel-parameters/6-channel-parameters-fields-added/after.yaml":oe,"../../../../samples/async-api-diffs/channel-parameters/7-channel-parameters-fields-removed/after.yaml":me,"../../../../samples/async-api-diffs/channel-parameters/8-channel-parameters-fields-changed/after.yaml":pe}),de=z(ie,ce),le=Object.fromEntries(de.map(e=>[e.caseId,e])),Y=({caseId:e})=>{const n=le[e];return n?d.jsx(D,{...he(n.beforeYaml,n.afterYaml)}):d.jsxs("div",{children:["Sample case not found: ",e]})},Ie={title:"Async API Diffs Suite/Channel Parameters Samples",component:Y},ge="sendOperation",fe="TestMessage",l=e=>H(e),he=(e,n)=>({devMode:!0,mergedSource:B({beforeSource:l(e),afterSource:l(n)}),operationKeys:{operationKey:ge,messageKey:fe},referenceNamePropertyKey:V,diffMetaKeys:G}),s=e=>({name:e,args:{caseId:e},render:n=>d.jsx(Y,{caseId:n.caseId}),play:async({canvasElement:n})=>{const R=await F(n).findAllByTestId("message-channel");await N.click(R[0])}}),a=s("1-channel-parameters-two-added"),r=s("2-channel-parameters-two-removed"),t=s("3-channel-parameters-second-added"),o=s("4-channel-parameters-second-removed"),m=s("5-channel-parameters-key-renamed"),p=s("6-channel-parameters-fields-added"),i=s("7-channel-parameters-fields-removed"),c=s("8-channel-parameters-fields-changed");var g,f,h;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:'createCaseStory("1-channel-parameters-two-added")',...(h=(f=a.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var _,y,T;r.parameters={...r.parameters,docs:{...(_=r.parameters)==null?void 0:_.docs,source:{originalSource:'createCaseStory("2-channel-parameters-two-removed")',...(T=(y=r.parameters)==null?void 0:y.docs)==null?void 0:T.source}}};var M,u,P;t.parameters={...t.parameters,docs:{...(M=t.parameters)==null?void 0:M.docs,source:{originalSource:'createCaseStory("3-channel-parameters-second-added")',...(P=(u=t.parameters)==null?void 0:u.docs)==null?void 0:P.source}}};var v,$,C;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:'createCaseStory("4-channel-parameters-second-removed")',...(C=($=o.parameters)==null?void 0:$.docs)==null?void 0:C.source}}};var b,S,A;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:'createCaseStory("5-channel-parameters-key-renamed")',...(A=(S=m.parameters)==null?void 0:S.docs)==null?void 0:A.source}}};var O,j,I;p.parameters={...p.parameters,docs:{...(O=p.parameters)==null?void 0:O.docs,source:{originalSource:'createCaseStory("6-channel-parameters-fields-added")',...(I=(j=p.parameters)==null?void 0:j.docs)==null?void 0:I.source}}};var x,E,w;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:'createCaseStory("7-channel-parameters-fields-removed")',...(w=(E=i.parameters)==null?void 0:E.docs)==null?void 0:w.source}}};var q,K,k;c.parameters={...c.parameters,docs:{...(q=c.parameters)==null?void 0:q.docs,source:{originalSource:'createCaseStory("8-channel-parameters-fields-changed")',...(k=(K=c.parameters)==null?void 0:K.docs)==null?void 0:k.source}}};const xe=["Case_1_channel_parameters_two_added","Case_2_channel_parameters_two_removed","Case_3_channel_parameters_second_added","Case_4_channel_parameters_second_removed","Case_5_channel_parameters_key_renamed","Case_6_channel_parameters_fields_added","Case_7_channel_parameters_fields_removed","Case_8_channel_parameters_fields_changed"];export{a as Case_1_channel_parameters_two_added,r as Case_2_channel_parameters_two_removed,t as Case_3_channel_parameters_second_added,o as Case_4_channel_parameters_second_removed,m as Case_5_channel_parameters_key_renamed,p as Case_6_channel_parameters_fields_added,i as Case_7_channel_parameters_fields_removed,c as Case_8_channel_parameters_fields_changed,xe as __namedExportsOrder,Ie as default};
