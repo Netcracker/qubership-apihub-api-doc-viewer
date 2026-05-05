17aaeba7132e70f4bf21236d3e36af449f4dbedf
JayLim2 <JayLim2@users.noreply.github.com>
2026-05-05 10:57:14 +0000

    deploy: 3f14d78e6d9de2fb8dde113070f7b35b248b13f9

--- featire/jso-diff-refactor/api-doc-viewer/src/components/JsoViewer/JsoValue/JsoValue.d.ts
+++ featire/jso-diff-refactor/api-doc-viewer/src/components/JsoViewer/JsoValue/JsoValue.d.ts
@@ -1,10 +1,10 @@
-import { HighlightVariant } from '../../../../../next-data-model/src/model/abstract/tree-with-diffs/tree-node.interface';
 import { FC } from '../../../../../../node_modules/react';
-import { ResolvedJsoSideState } from "../resolve-jso-side-state";
-type JsoValueProps = {
-    sideState: ResolvedJsoSideState;
-    forcedBackgroundColor?: HighlightVariant;
-    forcedTextHighlighterColor?: Exclude<HighlightVariant, HighlightVariant.Gray>;
+import { JsoValueAppearance } from "./useJsoValueStyles";
+export type JsoValueProps = {
+    isVisible: boolean;
+    value: unknown;
+    appearance: JsoValueAppearance;
+    textClassName?: string;
+    blockClassName?: string;
 };
 export declare const JsoValue: FC<JsoValueProps>;
-export {};
