4d9549c399adb8650a9e91f82b3244169d4b26b4
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-05-13 11:10:18 +0400

    feat: Basic node diffs calculations

--- packages/next-data-model/src/building-service/jso/tree-with-diffs/node-diffs-data/node-diffs/kind-any.ts
+++ packages/next-data-model/src/building-service/jso/tree-with-diffs/node-diffs-data/node-diffs/kind-any.ts
@@ -1,5 +1,5 @@
 import { AbstractNodeDiffsAggregator } from "@apihub/next-data-model/building-service/abstract/tree-with-diffs/node-diffs-data/node-diffs-aggregator";
-import { ChangedPropertyKey, DiffStyles, HighlightVariant, ITreeNodeWithDiffs, NodeDiffs } from "@apihub/next-data-model/model/abstract/tree-with-diffs/tree-node.interface";
+import { ChangedPropertyKey, DiffFlags, DiffStyles, HighlightVariant, ITreeNodeWithDiffs, NodeDiffs } from "@apihub/next-data-model/model/abstract/tree-with-diffs/tree-node.interface";
 import { JsoTreeNodeDiffsSource } from "@apihub/next-data-model/model/jso/tree-with-diffs/node-diffs-source";
 import { JsoTreeNodeValueWithDiffs } from "@apihub/next-data-model/model/jso/tree-with-diffs/node-value";
 import { JsoTreeNodeKind } from "@apihub/next-data-model/model/jso/types/node-kind";
@@ -7,6 +7,7 @@ import { JsoTreeNodeMeta } from "@apihub/next-data-model/model/jso/types/node-me
 import { isObject } from "@apihub/next-data-model/utilities";
 import { NodeKey } from "@apihub/next-data-model/utility-types";
 import { Diff, DiffType, isDiffAdd, isDiffRemove, isDiffRename, isDiffReplace } from "@netcracker/qubership-apihub-api-diff";
+import { JsoRawValueUtilities } from "../../../json-crawl-entities/transformers/raw-jso-property-to-base-jso-node-value";
 import { DiffMetaKeys } from "./factory";
 
 export class JsoNodeDiffsAggregatorKindAny
@@ -16,8 +17,13 @@ export class JsoNodeDiffsAggregatorKindAny
     JsoTreeNodeMeta,
     JsoTreeNodeDiffsSource
   > {
+
   private readonly DEFAULT_DIFF_STYLES: DiffStyles = {
     isContentVisible: true,
+  }
+
+  private readonly DEFAULT_DIFF_FLAGS: DiffFlags = {
+    increaseLevel: true,
   }
 
   private static readonly DIFFABLE_NODE_VALUE_KEYS: (keyof JsoTreeNodeDiffsSource)[] = [
@@ -61,15 +67,23 @@ export class JsoNodeDiffsAggregatorKindAny
   ) {
     let beforeStyles: DiffStyles = this.DEFAULT_DIFF_STYLES
     let afterStyles: DiffStyles = this.DEFAULT_DIFF_STYLES
+    let beforeFlags: DiffFlags = this.DEFAULT_DIFF_FLAGS
+    let afterFlags: DiffFlags = this.DEFAULT_DIFF_FLAGS
     if (isDiffAdd(diff)) {
       key = ''
       beforeStyles = {
         isContentVisible: false,
         backgroundColor: HighlightVariant.Gray,
       }
+      beforeFlags = {
+        increaseLevel: false,
+      }
       afterStyles = {
         isContentVisible: true,
         backgroundColor: HighlightVariant.Green,
+      }
+      afterFlags = {
+        increaseLevel: true,
       }
     }
     if (isDiffRemove(diff)) {
@@ -78,21 +92,49 @@ export class JsoNodeDiffsAggregatorKindAny
         isContentVisible: true,
         backgroundColor: HighlightVariant.Red,
       }
+      beforeFlags = {
+        increaseLevel: true,
+      }
       afterStyles = {
         isContentVisible: false,
         backgroundColor: HighlightVariant.Gray,
       }
+      afterFlags = {
+        increaseLevel: false,
+      }
     }
-    if (isDiffRename(diff) || isDiffReplace(diff)) {
+    if (isDiffReplace(diff)) {
+      const { beforeValue, afterValue } = diff
+      const beforeValueType = JsoRawValueUtilities.getValueType(beforeValue)
+      const afterValueType = JsoRawValueUtilities.getValueType(afterValue)
+      const isBeforeValuePrimitive = JsoRawValueUtilities.isPrimitiveValue(beforeValueType)
+      const isAfterValuePrimitive = JsoRawValueUtilities.isPrimitiveValue(afterValueType)
+      const isBeforeValuePredefined = JsoRawValueUtilities.isPredefinedValueSet(beforeValueType)
+      const isAfterValuePredefined = JsoRawValueUtilities.isPredefinedValueSet(afterValueType)
+      if (!isBeforeValuePrimitive || !isAfterValuePrimitive) {
+        key = '' // complex <-> primitive, complex <-> complex
+      }
       beforeStyles = {
-        isContentVisible: true,
+        isContentVisible: isBeforeValuePrimitive,
         backgroundColor: HighlightVariant.Yellow,
-        textHighlighterColor: HighlightVariant.Yellow,
+      }
+      if (isBeforeValuePrimitive) {
+        if (isBeforeValuePredefined) {
+          beforeStyles.borderShadowColor = HighlightVariant.Yellow
+        } else {
+          beforeStyles.textHighlighterColor = HighlightVariant.Yellow
+        }
       }
       afterStyles = {
-        isContentVisible: true,
+        isContentVisible: isAfterValuePrimitive,
         backgroundColor: HighlightVariant.Yellow,
-        textHighlighterColor: HighlightVariant.Yellow,
+      }
+      if (isAfterValuePrimitive) {
+        if (isAfterValuePredefined) {
+          afterStyles.borderShadowColor = HighlightVariant.Yellow
+        } else {
+          afterStyles.textHighlighterColor = HighlightVariant.Yellow
+        }
       }
     }
     nodeDiffs[key] = {
@@ -102,12 +144,8 @@ export class JsoNodeDiffsAggregatorKindAny
         after: afterStyles,
       },
       flags: {
-        before: {
-          increaseLevel: isDiffAdd(diff) || isDiffReplace(diff),
-        },
-        after: {
-          increaseLevel: isDiffRemove(diff) || isDiffReplace(diff),
-        },
+        before: beforeFlags,
+        after: afterFlags,
       },
     }
   }
