58f451a9f9b2b6d8fa89d99cdda1ad3673809cc4
JayLim2 <JayLim2@users.noreply.github.com>
2026-05-14 12:55:04 +0000

    deploy: b60ef6aec8913c2fdb8486f61fb0e27cf4db438e

--- feature/jso-diff-refactor/api-doc-viewer/src/components/JsoViewer/JsoValue/useJsoValueStyles.d.ts
+++ feature/jso-diff-refactor/api-doc-viewer/src/components/JsoViewer/JsoValue/useJsoValueStyles.d.ts
@@ -1,7 +1,5 @@
 export type JsoValueAppearance = 'text' | 'block';
 export type UseJsoValueStylesInput = {
     appearance: JsoValueAppearance;
-    textClassName?: string;
-    blockClassName?: string;
 };
 export declare function useJsoValueStyles(input: UseJsoValueStylesInput): string;
