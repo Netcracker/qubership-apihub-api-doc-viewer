413bb5c237f2ffc6cdf164b87a379a1fd109539c
NetcrackerCLPLCI <NetcrackerCLPLCI@users.noreply.github.com>
2026-05-14 10:35:12 +0000

    deploy: 9f13cd483a37fe7b66db789e2cd0decdc5787a0c

---
+++ broadcast-cla-workflow/api-doc-viewer/src/components/AsyncApiOperationViewer/AddressRow.d.ts
@@ -0,0 +1,7 @@
+import { FC } from '../../../../../node_modules/react';
+type AddressRowProps = {
+    action: string;
+    address: string;
+};
+export declare const AddressRow: FC<AddressRowProps>;
+export {};
