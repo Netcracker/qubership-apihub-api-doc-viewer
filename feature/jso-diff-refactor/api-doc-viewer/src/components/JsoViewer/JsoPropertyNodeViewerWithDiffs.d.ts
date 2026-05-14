61fff5b990803cd10f0602b466a1efa8125bcdf4
JayLim2 <JayLim2@users.noreply.github.com>
2026-05-14 10:23:09 +0000

    deploy: 82557e3b71c197c176535825e82fdff2655c7a7b

--- feature/jso-diff-refactor/api-doc-viewer/src/components/JsoViewer/JsoPropertyNodeViewerWithDiffs.d.ts
+++ feature/jso-diff-refactor/api-doc-viewer/src/components/JsoViewer/JsoPropertyNodeViewerWithDiffs.d.ts
@@ -1,8 +1,7 @@
 import { JsoTreeNodeWithDiffs } from '../../../../next-data-model/src/model/jso/types/aliases';
-import { JsoTreeNodeKinds } from '../../../../next-data-model/src/model/jso/types/node-kind';
 import { FC } from '../../../../../node_modules/react';
 type JsoPropertyNodeViewerWithDiffsProps = {
-    node: JsoTreeNodeWithDiffs<typeof JsoTreeNodeKinds.PROPERTY>;
+    node: JsoTreeNodeWithDiffs;
     supportJsonSchema?: boolean;
 };
 export declare const JsoPropertyNodeViewerWithDiffs: FC<JsoPropertyNodeViewerWithDiffsProps>;
