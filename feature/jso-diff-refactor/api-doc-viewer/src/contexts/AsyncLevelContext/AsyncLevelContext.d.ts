4511e6ea6023c081bafc2b0f84fecdb9be650449
JayLim2 <JayLim2@users.noreply.github.com>
2026-05-15 15:37:23 +0000

    deploy: f29ae49fd8a0c36e2968769ad117b72d7e216d3b

--- feature/jso-diff-refactor/api-doc-viewer/src/contexts/AsyncLevelContext/AsyncLevelContext.d.ts
+++ feature/jso-diff-refactor/api-doc-viewer/src/contexts/AsyncLevelContext/AsyncLevelContext.d.ts
@@ -2,5 +2,5 @@ export type AsyncLevelContextValue = {
     beforeLevel: number;
     afterLevel: number;
 };
-export declare const AsyncLevelContext: import('../../../../../node_modules/react').Context<AsyncLevelContextValue>;
-export declare function useAsyncLevelContext(): AsyncLevelContextValue;
+export declare const AsyncLevelContext: import('../../../../../node_modules/react').Context<AsyncLevelContextValue | undefined>;
+export declare function useAsyncLevelContext(): AsyncLevelContextValue | undefined;
