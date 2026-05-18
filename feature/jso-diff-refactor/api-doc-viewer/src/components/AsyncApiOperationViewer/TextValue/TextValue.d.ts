f9dc5472c582f83c9ccb20e6952182c8d99312b8
JayLim2 <JayLim2@users.noreply.github.com>
2026-05-18 15:18:55 +0000

    deploy: 698284fbef0be65ed427ce29cb89589a710a235a

--- feature/jso-diff-refactor/api-doc-viewer/src/components/AsyncApiOperationViewer/TextValue/TextValue.d.ts
+++ feature/jso-diff-refactor/api-doc-viewer/src/components/AsyncApiOperationViewer/TextValue/TextValue.d.ts
@@ -2,6 +2,7 @@ import { DescriptionFontSize } from '../../common/annotations/Description/types/
 import { ChangedPropertyMetaData, DiffHighlightingApplicationMode } from '../../../../../next-data-model/src/model/abstract/tree-with-diffs/tree-node.interface';
 import { LayoutSide } from '../../../types/internal/LayoutSide';
 import { Dispatch, FC, SetStateAction } from '../../../../../../node_modules/react';
+import { TitleRowUsage } from "../TitleRow/types";
 import { TextValueVariant } from "./types";
 type TextValueProps = {
     value?: string;
@@ -11,6 +12,7 @@ type TextValueProps = {
     fontWeight?: 'normal' | 'bold';
     label?: string;
     diff?: ChangedPropertyMetaData;
+    usage?: TitleRowUsage;
     highlightingMode?: DiffHighlightingApplicationMode;
 };
 export type TextExpanderProps = Partial<{
