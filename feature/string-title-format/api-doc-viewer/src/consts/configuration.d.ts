7efb1bd6d51c2472b2575d1716c6bb0eb8776731
b41ex <b41ex@users.noreply.github.com>
2026-05-19 18:00:00 +0000

    deploy: fd6832e46b4851eae645f7b356210a7eaf720db5

---
+++ feature/string-title-format/api-doc-viewer/src/consts/configuration.d.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2024-2025 NetCracker Technology Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export declare const DEFAULT_EXPANDED_DEPTH: number;
+export declare const INFINITY_EXPANDING_DEPTH: number;
+export declare const DIFF_META_KEY: unique symbol;
+export declare const DEFAULT_DISPLAY_MODE = "detailed";
+export declare const DEFAULT_LAYOUT_MODE = "document";
+export declare const DEFAULT_ROW_DEPTH = 0;
+export declare const DEFAULT_ROW_PADDING_LEFT = "pl-2";
+export declare const SHIFTED_ROW_PADDING_LEFT = "pl-7";
+export declare const DEFAULT_SERIES_ITEM = "<empty string>";
+export declare const DEFAULT_SERIES_ITEM_TEXT_COLOR = "text-gray-400/[.8]";
