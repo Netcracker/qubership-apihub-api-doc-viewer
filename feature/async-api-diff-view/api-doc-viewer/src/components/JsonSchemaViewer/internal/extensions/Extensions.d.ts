6bdcf34089b82b40dfad7e649d4b36f1abcf63ff
JayLim2 <JayLim2@users.noreply.github.com>
2026-06-01 12:19:12 +0000

    deploy: 61a6ab3031accbbef3015780a15fbb4d5e9654b2

--- feature/async-api-diff-view/api-doc-viewer/src/components/JsonSchemaViewer/internal/extensions/Extensions.d.ts
+++ feature/async-api-diff-view/api-doc-viewer/src/components/JsonSchemaViewer/internal/extensions/Extensions.d.ts
@@ -1,7 +1,8 @@
-import { IJsonSchemaBaseType } from '../../../../../../api-data-model/src';
+import { IJsonSchemaBaseType, NodeChange } from '../../../../../../api-data-model/src';
 import { FC } from '../../../../../../../node_modules/react';
 type ExtensionsProps = {
     extensions: NonNullable<IJsonSchemaBaseType['extensions']>;
+    $nodeChange?: NodeChange;
 };
 export declare const Extensions: FC<ExtensionsProps>;
 export {};
