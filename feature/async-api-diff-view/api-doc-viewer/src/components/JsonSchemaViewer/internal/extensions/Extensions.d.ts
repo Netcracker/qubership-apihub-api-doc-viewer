8a3f6a0425c79d0e0a2e299b7ab072cd20d7c11d
JayLim2 <JayLim2@users.noreply.github.com>
2026-06-01 09:01:43 +0000

    deploy: ef538ba8eb65a7d338b092fbc8964fd532f3d12f

--- feature/async-api-diff-view/api-doc-viewer/src/components/JsonSchemaViewer/internal/extensions/Extensions.d.ts
+++ feature/async-api-diff-view/api-doc-viewer/src/components/JsonSchemaViewer/internal/extensions/Extensions.d.ts
@@ -1,7 +1,6 @@
 import { IJsonSchemaBaseType } from '../../../../../../api-data-model/src';
 import { FC } from '../../../../../../../node_modules/react';
 type ExtensionsProps = {
-    shift?: boolean;
     extensions: NonNullable<IJsonSchemaBaseType['extensions']>;
 };
 export declare const Extensions: FC<ExtensionsProps>;
---
+++ feature/async-api-diff-view/assets/channel-parameters-samples.stories-588e2d1a.js
@@ -0,0 +1,595 @@
+import{j as d}from"./_commonjs-dynamic-modules-6308e768.js";import{a as D}from"./AsyncApiOperationViewer-0b92ae7e.js";import{w as F,u as L}from"./index-d5b0668c.js";import{b as N,T as B,a as V}from"./preprocess-56596757.js";import{c as G,p as z}from"./yaml-source-2575e8ef.js";import"./index-f46741a2.js";import"./GraphPropNodeViewer-6fa6d307.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-9c68a81b.js";import"./GraphQLOperationDiffViewer-5477d4ab.js";import"./index-442a5f79.js";import"./js-yaml-71a24e1f.js";const H=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.static
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,J=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}.{param2}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+        location: $message.payload#/id
+        schema:
+          type: string
+          minLength: 1
+      param2:
+        description: second parameter
+        location: $message.payload#/id
+        schema:
+          type: string
+          minLength: 1
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,Q=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,U=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}.{param2}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+      param2:
+        description: second parameter
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,W=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}.{param2}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+      param2:
+        description: second parameter
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,X=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{firstParam}.{secondParam}.{thirdParam}.{fourthParam}.{fifthParam}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      firstParam:
+        description: first parameter
+      secondParam:
+        location: $message.payload#/id
+      thirdParam:
+        location: $message.payload#/id
+      fourthParam:
+        location: $message.payload#/id
+      fifthParam:
+        location: $message.payload#/id
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,Z=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{firstParam}.{secondParam}.{thirdParam}.{fourthParam}.{fifthParam}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      firstParam:
+        description: first parameter
+        location: $message.header#/id
+      secondParam:
+        description: second parameter
+        location: $message.payload#/id
+      thirdParam:
+        location: $message.payload#/id
+        enum:
+          - alpha
+          - beta
+      fourthParam:
+        location: $message.payload#/id
+        default: default-value
+      fifthParam:
+        location: $message.payload#/id
+        examples:
+          - example-1
+          - example-2
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,ee=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{firstParam}.{secondParam}.{thirdParam}.{fourthParam}.{fifthParam}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      firstParam:
+        description: first parameter
+        location: $message.payload#/id
+      secondParam:
+        description: second parameter
+      thirdParam:
+        enum:
+          - alpha
+          - beta
+      fourthParam:
+        default: default-value
+      fifthParam:
+        examples:
+          - example-1
+          - example-2
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,ne=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}.{param2}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+        location: $message.payload#/id
+        schema:
+          type: string
+          minLength: 1
+      param2:
+        description: second parameter
+        location: $message.payload#/id
+        schema:
+          type: string
+          minLength: 1
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,se=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.static
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,ae=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}.{param2}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+      param2:
+        description: second parameter
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,te=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{param1}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      param1:
+        description: first parameter
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,re=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{parameter_1}.{param2}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      parameter_1:
+        description: first parameter
+      param2:
+        description: second parameter
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,oe=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{firstParam}.{secondParam}.{thirdParam}.{fourthParam}.{fifthParam}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      firstParam:
+        description: first parameter
+        location: $message.header#/id
+      secondParam:
+        description: second parameter
+        location: $message.payload#/id
+      thirdParam:
+        location: $message.payload#/id
+        enum:
+          - alpha
+          - beta
+      fourthParam:
+        location: $message.payload#/id
+        default: default-value
+      fifthParam:
+        location: $message.payload#/id
+        examples:
+          - example-1
+          - example-2
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,me=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{firstParam}.{secondParam}.{thirdParam}.{fourthParam}.{fifthParam}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      firstParam:
+        description: first parameter
+      secondParam:
+        location: $message.payload#/id
+      thirdParam:
+        location: $message.payload#/id
+      fourthParam:
+        location: $message.payload#/id
+      fifthParam:
+        location: $message.payload#/id
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,pe=`asyncapi: 3.0.0
+info:
+  title: Sample AsyncAPI
+  version: 1.0.0
+channels:
+  testChannel:
+    address: events.{firstParam}.{secondParam}.{thirdParam}.{fourthParam}.{fifthParam}
+    messages:
+      TestMessage:
+        $ref: '#/components/messages/TestMessage'
+    parameters:
+      firstParam:
+        description: first parameter
+        location: $message.header#/id
+      secondParam:
+        description: updated second parameter description
+      thirdParam:
+        enum:
+          - alpha
+          - gamma
+      fourthParam:
+        default: changed-default-value
+      fifthParam:
+        examples:
+          - example-1
+          - changed-example-2
+operations:
+  sendOperation:
+    action: send
+    channel:
+      $ref: '#/channels/testChannel'
+    messages:
+      - $ref: '#/components/messages/TestMessage'
+components:
+  messages:
+    TestMessage:
+      name: TestMessage
+      payload:
+        type: object
+        properties:
+          id:
+            type: string
+        required:
+          - id
+`,ie=Object.assign({"../../../../samples/async-api-diffs/channel-parameters/1-channel-parameters-two-added/before.yaml":H,"../../../../samples/async-api-diffs/channel-parameters/2-channel-parameters-two-removed/before.yaml":J,"../../../../samples/async-api-diffs/channel-parameters/3-channel-parameters-second-added/before.yaml":Q,"../../../../samples/async-api-diffs/channel-parameters/4-channel-parameters-second-removed/before.yaml":U,"../../../../samples/async-api-diffs/channel-parameters/5-channel-parameters-key-renamed/before.yaml":W,"../../../../samples/async-api-diffs/channel-parameters/6-channel-parameters-fields-added/before.yaml":X,"../../../../samples/async-api-diffs/channel-parameters/7-channel-parameters-fields-removed/before.yaml":Z,"../../../../samples/async-api-diffs/channel-parameters/8-channel-parameters-fields-changed/before.yaml":ee}),ce=Object.assign({"../../../../samples/async-api-diffs/channel-parameters/1-channel-parameters-two-added/after.yaml":ne,"../../../../samples/async-api-diffs/channel-parameters/2-channel-parameters-two-removed/after.yaml":se,"../../../../samples/async-api-diffs/channel-parameters/3-channel-parameters-second-added/after.yaml":ae,"../../../../samples/async-api-diffs/channel-parameters/4-channel-parameters-second-removed/after.yaml":te,"../../../../samples/async-api-diffs/channel-parameters/5-channel-parameters-key-renamed/after.yaml":re,"../../../../samples/async-api-diffs/channel-parameters/6-channel-parameters-fields-added/after.yaml":oe,"../../../../samples/async-api-diffs/channel-parameters/7-channel-parameters-fields-removed/after.yaml":me,"../../../../samples/async-api-diffs/channel-parameters/8-channel-parameters-fields-changed/after.yaml":pe}),de=G(ie,ce),le=Object.fromEntries(de.map(e=>[e.caseId,e])),Y=({caseId:e})=>{const n=le[e];return n?d.jsx(D,{...he(n.beforeYaml,n.afterYaml)}):d.jsxs("div",{children:["Sample case not found: ",e]})},je={title:"Async API Diffs Suite/Channel Parameters Samples",component:Y},ge="sendOperation",fe="TestMessage",l=e=>z(e),he=(e,n)=>({devMode:!0,mergedSource:N({beforeSource:l(e),afterSource:l(n)}),operationKeys:{operationKey:ge,messageKey:fe},referenceNamePropertyKey:B,diffMetaKeys:V}),s=e=>({name:e,args:{caseId:e},render:n=>d.jsx(Y,{caseId:n.caseId}),play:async({canvasElement:n})=>{const R=await F(n).findAllByTestId("message-channel");await L.click(R[0])}}),a=s("1-channel-parameters-two-added"),t=s("2-channel-parameters-two-removed"),r=s("3-channel-parameters-second-added"),o=s("4-channel-parameters-second-removed"),m=s("5-channel-parameters-key-renamed"),p=s("6-channel-parameters-fields-added"),i=s("7-channel-parameters-fields-removed"),c=s("8-channel-parameters-fields-changed");var g,f,h;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:'createCaseStory("1-channel-parameters-two-added")',...(h=(f=a.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var _,y,T;t.parameters={...t.parameters,docs:{...(_=t.parameters)==null?void 0:_.docs,source:{originalSource:'createCaseStory("2-channel-parameters-two-removed")',...(T=(y=t.parameters)==null?void 0:y.docs)==null?void 0:T.source}}};var M,u,P;r.parameters={...r.parameters,docs:{...(M=r.parameters)==null?void 0:M.docs,source:{originalSource:'createCaseStory("3-channel-parameters-second-added")',...(P=(u=r.parameters)==null?void 0:u.docs)==null?void 0:P.source}}};var v,$,C;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:'createCaseStory("4-channel-parameters-second-removed")',...(C=($=o.parameters)==null?void 0:$.docs)==null?void 0:C.source}}};var b,S,A;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:'createCaseStory("5-channel-parameters-key-renamed")',...(A=(S=m.parameters)==null?void 0:S.docs)==null?void 0:A.source}}};var O,j,I;p.parameters={...p.parameters,docs:{...(O=p.parameters)==null?void 0:O.docs,source:{originalSource:'createCaseStory("6-channel-parameters-fields-added")',...(I=(j=p.parameters)==null?void 0:j.docs)==null?void 0:I.source}}};var x,E,w;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:'createCaseStory("7-channel-parameters-fields-removed")',...(w=(E=i.parameters)==null?void 0:E.docs)==null?void 0:w.source}}};var q,K,k;c.parameters={...c.parameters,docs:{...(q=c.parameters)==null?void 0:q.docs,source:{originalSource:'createCaseStory("8-channel-parameters-fields-changed")',...(k=(K=c.parameters)==null?void 0:K.docs)==null?void 0:k.source}}};const Ie=["Case_1_channel_parameters_two_added","Case_2_channel_parameters_two_removed","Case_3_channel_parameters_second_added","Case_4_channel_parameters_second_removed","Case_5_channel_parameters_key_renamed","Case_6_channel_parameters_fields_added","Case_7_channel_parameters_fields_removed","Case_8_channel_parameters_fields_changed"];export{a as Case_1_channel_parameters_two_added,t as Case_2_channel_parameters_two_removed,r as Case_3_channel_parameters_second_added,o as Case_4_channel_parameters_second_removed,m as Case_5_channel_parameters_key_renamed,p as Case_6_channel_parameters_fields_added,i as Case_7_channel_parameters_fields_removed,c as Case_8_channel_parameters_fields_changed,Ie as __namedExportsOrder,je as default};
