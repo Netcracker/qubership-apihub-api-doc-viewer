baf94eefe7930088db100baaeaccc3f73134ae4a
JayLim2 <JayLim2@users.noreply.github.com>
2026-06-05 13:38:09 +0000

    deploy: 879b95721ec0022d32cc116a1e46209d4b48d8a9

--- feature/async-api-diff-view/api-doc-viewer/src/utils/jso/prepare-json-schema-to-jso-viewers.d.ts
+++ feature/async-api-diff-view/api-doc-viewer/src/utils/jso/prepare-json-schema-to-jso-viewers.d.ts
@@ -3,5 +3,7 @@ import { ChangedPropertyMetaData } from '../../../../next-data-model/src/model/a
 import { JsoTreeNodeValueWithDiffs } from '../../../../next-data-model/src/model/jso/tree-with-diffs/node-value';
 import { JsoTreeNodeValueBase } from '../../../../next-data-model/src/model/jso/tree/node-value';
 import { NodeKey } from '../../../../next-data-model/src/utility-types';
+export declare function wrapJsonSchemaForViewer(nodeKey: NodeKey, schema: object | undefined): object | undefined;
+export declare function wrapJsonSchemaForDiffsViewer(nodeKey: NodeKey, schema: object | undefined, nodeValueDiff: ChangedPropertyMetaData | undefined, diffMetaKeys: DiffMetaKeys | undefined): object | undefined;
 export declare function prepareJsonSchemaForJsoViewer(nodeKey: NodeKey, nodeValue: JsoTreeNodeValueBase | null | undefined): object | undefined;
 export declare function prepareJsonSchemaForJsoDiffsViewer(nodeKey: NodeKey, nodeValue: JsoTreeNodeValueWithDiffs | null | undefined, nodeValueDiff: ChangedPropertyMetaData | undefined, diffMetaKeys: DiffMetaKeys | undefined): object | undefined;
