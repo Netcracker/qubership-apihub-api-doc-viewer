f5164adf05814806408b924ad4a6b171f55c83c3
b41ex <b41ex@users.noreply.github.com>
2026-06-07 19:37:52 +0000

    deploy: fb1ac62e025a77922106bf06e88bc2cad1587718

---
+++ feature/string-title-format-2/api-doc-viewer/src/contexts/CustomizationOptionsContext.d.ts
@@ -0,0 +1,5 @@
+export declare const CustomizationOptionsContext: import('../../../../node_modules/react').Context<CustomizationOptions | undefined>;
+export declare function useCustomizationOptions(): CustomizationOptions | undefined;
+export type CustomizationOptions = {
+    headerRowTitle?: string;
+};
