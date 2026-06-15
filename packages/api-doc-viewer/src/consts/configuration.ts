3736ab526e3dedd5135f695cfd2adee1e64ce028
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-06-15 13:12:43 +0400

    fix: Update indentations

--- packages/api-doc-viewer/src/consts/configuration.ts
+++ packages/api-doc-viewer/src/consts/configuration.ts
@@ -27,8 +27,8 @@ export const DEFAULT_ROW_DEPTH = 0
 
 // Layout
 
-export const DEFAULT_ROW_PADDING_LEFT = 'pl-2'
-export const SHIFTED_ROW_PADDING_LEFT = 'pl-7'
+export const DEFAULT_ROW_PADDING_LEFT = 'pl-4'
+export const SHIFTED_ROW_PADDING_LEFT = 'pl-9'
 
 // Defaults & Placeholders
 
