8bb28a977768d2034a771ff1fe963084798da47f
b41ex <b41ex@users.noreply.github.com>
2026-06-23 15:34:40 +0000

    deploy: 69636983f1e350f39837ae243ac4f3d542b19104

---
+++ bugfix/system-converts-table-to-string-2/api-doc-viewer/src/stories/jso-diffs-suite/objectJsonSchema-samples.stories.d.ts
@@ -0,0 +1,17 @@
+import { type JsoPropertySamplesStoryObj } from "./property-samples-common";
+declare const meta: {
+    title: string;
+    component: ({ caseId }: import("./property-samples-common").JsoPropertySamplesStoryProps) => import('../../../../../node_modules/react/jsx-runtime').JSX.Element;
+};
+export default meta;
+type Story = JsoPropertySamplesStoryObj;
+export declare const Case_13_2_objectJsonSchema_add_complex_json_schema: Story;
+export declare const Case_13_4_objectJsonSchema_remove_complex_json_schema: Story;
+export declare const Case_15_1_objectJsonSchema_description_changed: Story;
+export declare const Case_15_2_objectJsonSchema_title_changed: Story;
+export declare const Case_15_3_objectJsonSchema_title_added: Story;
+export declare const Case_15_4_objectJsonSchema_title_removed: Story;
+export declare const Case_15_5_objectJsonSchema_examples_changed: Story;
+export declare const Case_15_6_objectJsonSchema_property_added: Story;
+export declare const Case_15_7_objectJsonSchema_property_removed: Story;
+export declare const Case_15_8_objectJsonSchema_required_changed: Story;
