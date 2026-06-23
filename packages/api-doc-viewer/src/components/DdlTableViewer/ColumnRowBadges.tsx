d333923cd5ef8c26340c2f924967345beedde1e6
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-06-23 13:05:59 +0400

    feat: Reorder column badges, implement ForeignKey with correct styles

--- packages/api-doc-viewer/src/components/DdlTableViewer/ColumnRowBadges.tsx
+++ packages/api-doc-viewer/src/components/DdlTableViewer/ColumnRowBadges.tsx
@@ -1,9 +1,8 @@
 import { DdlApiColumnRowValue } from "@netcracker/qubership-apihub-next-data-model/model/ddlapi/tree/node-value"
-import { FC, memo, useCallback } from "react"
+import { FC, memo } from "react"
 import { UxBadge } from "../kit/ux/UxBadge/UxBadge"
-import { useDdlTableViewerContext } from "./DdlTableViewerContext"
-import { DDL_API_FOREIGN_KEY_BADGE_COLOR_SCHEMA, DDL_API_GENERATED_BADGE_COLOR_SCHEMA, DDL_API_NOT_NULL_BADGE_COLOR_SCHEMA, DDL_API_PRIMARY_KEY_BADGE_COLOR_SCHEMA, DDL_API_UNIQUE_BADGE_COLOR_SCHEMA } from "./conts"
-import { formatForeignKeyTarget } from "./formatters"
+import { DDL_API_GENERATED_BADGE_COLOR_SCHEMA, DDL_API_NOT_NULL_BADGE_COLOR_SCHEMA, DDL_API_PRIMARY_KEY_BADGE_COLOR_SCHEMA, DDL_API_UNIQUE_BADGE_COLOR_SCHEMA } from "./conts"
+import { ForeignKey } from "./ForeignKey/ForeignKey"
 
 type ColumnRowBadgesProps = {
   value: DdlApiColumnRowValue
@@ -11,36 +10,17 @@ type ColumnRowBadgesProps = {
 
 export const ColumnRowBadges: FC<ColumnRowBadgesProps> = memo<ColumnRowBadgesProps>(props => {
   const { value } = props
-  const { navigationCallback } = useDdlTableViewerContext()
-
-  const onForeignKeyClick = useCallback(() => {
-    const target = value.foreignKeyTarget
-    if (!target) {
-      return
-    }
-    navigationCallback(target.schemaName, target.tableName, target.columnName)
-  }, [navigationCallback, value.foreignKeyTarget])
 
   return (
-    <div className="flex flex-wrap items-center gap-1">
+    <div className="flex flex-wrap items-center gap-2">
       {value.isPrimaryKey && (
         <UxBadge text="PK" colorSchema={DDL_API_PRIMARY_KEY_BADGE_COLOR_SCHEMA} inline />
       )}
-      {value.isForeignKey && value.foreignKeyTarget && (
-        <button
-          type="button"
-          className="inline-flex border-0 bg-transparent p-0"
-          onClick={onForeignKeyClick}
-        >
-          <UxBadge
-            text={`FK (${formatForeignKeyTarget(value.foreignKeyTarget)})`}
-            colorSchema={DDL_API_FOREIGN_KEY_BADGE_COLOR_SCHEMA}
-            inline
-          />
-        </button>
+      {value.isUnique && (
+        <UxBadge text="unique" colorSchema={DDL_API_UNIQUE_BADGE_COLOR_SCHEMA} inline />
       )}
-      {value.isForeignKey && !value.foreignKeyTarget && (
-        <UxBadge text="FK" colorSchema={DDL_API_FOREIGN_KEY_BADGE_COLOR_SCHEMA} inline />
+      {value.isNotNull && (
+        <UxBadge text="not null" colorSchema={DDL_API_NOT_NULL_BADGE_COLOR_SCHEMA} inline />
       )}
       {value.isGenerated && (
         <UxBadge
@@ -49,11 +29,8 @@ export const ColumnRowBadges: FC<ColumnRowBadgesProps> = memo<ColumnRowBadgesPro
           inline
         />
       )}
-      {value.isUnique && (
-        <UxBadge text="unique" colorSchema={DDL_API_UNIQUE_BADGE_COLOR_SCHEMA} inline />
-      )}
-      {value.isNotNull && (
-        <UxBadge text="not null" colorSchema={DDL_API_NOT_NULL_BADGE_COLOR_SCHEMA} inline />
+      {value.isForeignKey && value.foreignKeyTarget && (
+        <ForeignKey target={value.foreignKeyTarget} />
       )}
     </div>
   )
---
+++ packages/api-doc-viewer/src/components/DdlTableViewer/ForeignKey/ForeignKey.tsx
@@ -0,0 +1,31 @@
+import { DdlApiForeignKeyTarget } from "@netcracker/qubership-apihub-next-data-model/model/ddlapi/tree/node-value"
+import { FC, memo, MouseEvent, useCallback } from "react"
+import { UxBadge } from "../../kit/ux/UxBadge/UxBadge"
+import { useDdlTableViewerContext } from "../DdlTableViewerContext"
+import { DDL_API_FOREIGN_KEY_BADGE_COLOR_SCHEMA } from "../conts"
+import { formatForeignKeyTarget } from "../formatters"
+import './ForeignKey.css'
+
+type ForeignKeyProps = {
+  target: DdlApiForeignKeyTarget
+}
+
+export const ForeignKey: FC<ForeignKeyProps> = memo<ForeignKeyProps>(props => {
+  const { target } = props
+  const { navigationCallback } = useDdlTableViewerContext()
+
+  const onLinkClick = useCallback((event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault()
+    event.stopPropagation()
+    navigationCallback(target.schemaName, target.tableName, target.columnName)
+  }, [navigationCallback, target])
+
+  return (
+    <div className="ddl-api-foreign-key inline-flex flex-row items-center gap-1">
+      <UxBadge text="FK" colorSchema={DDL_API_FOREIGN_KEY_BADGE_COLOR_SCHEMA} inline />
+      <a href="#" className="ddl-api-foreign-key-link" onClick={onLinkClick}>
+        {formatForeignKeyTarget(target)}
+      </a>
+    </div>
+  )
+})
