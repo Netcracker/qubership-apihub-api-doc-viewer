3a3bf4c6645282d62f10ff5b6048db46f046e44c
JayLim2 <JayLim2@users.noreply.github.com>
2026-07-02 10:50:21 +0000

    deploy: 42e189164dc5dc2feaf49e92ad72f84d315699a6

---
+++ feature/ddl-diffs-view/api-doc-viewer/src/components/DdlTableViewer/ForeignKey/ForeignKey.d.ts
@@ -0,0 +1,7 @@
+import { DdlApiForeignKeyTarget } from '../../../../../next-data-model/src/model/ddlapi/tree/node-value';
+import { FC } from '../../../../../../node_modules/react';
+type ForeignKeyProps = {
+    target: DdlApiForeignKeyTarget;
+};
+export declare const ForeignKey: FC<ForeignKeyProps>;
+export {};
---
+++ feature/ddl-diffs-view/api-doc-viewer/src/components/common/NestingHorizontalIndicator.d.ts
@@ -0,0 +1,4 @@
+import { FC } from '../../../../../node_modules/react';
+export declare const NestingHorizontalIndicator: FC<{
+    short?: boolean;
+}>;
