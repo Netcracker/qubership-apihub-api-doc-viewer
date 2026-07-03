b46d3587d2de5febdd38c7a9f2875cb9e256833a
JayLim2 <JayLim2@users.noreply.github.com>
2026-07-03 09:52:25 +0000

    deploy: 625457b4f4570814595292665eefb570f333eee5

---
+++ feature/ddl-diffs-view/assets/graphql-compatibility-suite-root-type-general.generated.stories-f583c1ae.js
@@ -0,0 +1,16 @@
+import"./AsyncApiOperationViewer-6b6e9404.js";import"./DdlTableDiffsViewer-f3b6bf14.js";import"./DdlTableViewer-4c3a3e60.js";import"./GraphQLOperationDiffViewer-54a4e89c.js";import"./GraphQLOperationViewer-14cec534.js";import"./UxBadge-d0e51bff.js";import{G as R,g as e,T as r}from"./compatibility-suite-utils-05c6a21b.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./index-abe66225.js";/* empty css              */import"./GraphPropNodeViewer-76557377.js";import"./index-415bee12.js";import"./graph-api-transformers-82fa6db5.js";import"./buildASTSchema-f14864f0.js";const q={id:"graphql-compatibility-suite-root-type-general",title:"GraphQL Compatibility Suite/root-type-general",render:R},o="root-type-general",t={name:"add-description-for-root-type",args:e(r,o,"add-description-for-root-type")},a={name:"add-new-root-type",args:e(r,o,"add-new-root-type")},p={name:"change-description-for-root-type",args:e(r,o,"change-description-for-root-type")},s={name:"delete-description-for-root-type",args:e(r,o,"delete-description-for-root-type")},n={name:"delete-root-type",args:e(r,o,"delete-root-type")};var i,d,c;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  name: 'add-description-for-root-type',
+  args: getGraphQLStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, SUITE_ID, 'add-description-for-root-type')
+}`,...(c=(d=t.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var m,y,g;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  name: 'add-new-root-type',
+  args: getGraphQLStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, SUITE_ID, 'add-new-root-type')
+}`,...(g=(y=a.parameters)==null?void 0:y.docs)==null?void 0:g.source}}};var T,_,S;p.parameters={...p.parameters,docs:{...(T=p.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  name: 'change-description-for-root-type',
+  args: getGraphQLStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, SUITE_ID, 'change-description-for-root-type')
+}`,...(S=(_=p.parameters)==null?void 0:_.docs)==null?void 0:S.source}}};var l,E,u;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: 'delete-description-for-root-type',
+  args: getGraphQLStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, SUITE_ID, 'delete-description-for-root-type')
+}`,...(u=(E=s.parameters)==null?void 0:E.docs)==null?void 0:u.source}}};var P,A,D;n.parameters={...n.parameters,docs:{...(P=n.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  name: 'delete-root-type',
+  args: getGraphQLStoryArgs(TEST_SPEC_TYPE_GRAPH_QL, SUITE_ID, 'delete-root-type')
+}`,...(D=(A=n.parameters)==null?void 0:A.docs)==null?void 0:D.source}}};const O=["AddDescriptionForRootType","AddNewRootType","ChangeDescriptionForRootType","DeleteDescriptionForRootType","DeleteRootType"];export{t as AddDescriptionForRootType,a as AddNewRootType,p as ChangeDescriptionForRootType,s as DeleteDescriptionForRootType,n as DeleteRootType,O as __namedExportsOrder,q as default};
---
+++ feature/ddl-diffs-view/assets/whole-table-changes-samples.stories-e53fe35d.js
@@ -0,0 +1,23 @@
+import{D as p}from"./DdlTableDiffsViewer-f3b6bf14.js";import{c as b,a as f,b as h}from"./ddlapi-diffs-utils-996419bc.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./UxBadge-d0e51bff.js";import"./index-abe66225.js";/* empty css              */import"./build-from-ddl-browser-0b1e92b2.js";import"./iframe-a28047f7.js";import"../sb-preview/runtime.js";const E=`CREATE SCHEMA IF NOT EXISTS public;
+`,C=`CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE TABLE public.t (
+  id integer
+);
+`,S=`CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE TABLE public.t (
+  id integer
+);
+`,g=`CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE TABLE public.t (
+  id integer
+);
+`,T=`CREATE SCHEMA IF NOT EXISTS public;
+`,u=`CREATE SCHEMA IF NOT EXISTS audit;
+
+CREATE TABLE audit.t (
+  id integer
+);
+`,A=Object.assign({"../../../../samples/ddlapi-diffs/whole-table-changes/01-wholly-added-table/before.sql":E,"../../../../samples/ddlapi-diffs/whole-table-changes/02-wholly-removed-table/before.sql":C,"../../../../samples/ddlapi-diffs/whole-table-changes/03-changed-schema-of-table/before.sql":S}),y=Object.assign({"../../../../samples/ddlapi-diffs/whole-table-changes/01-wholly-added-table/after.sql":g,"../../../../samples/ddlapi-diffs/whole-table-changes/02-wholly-removed-table/after.sql":T,"../../../../samples/ddlapi-diffs/whole-table-changes/03-changed-schema-of-table/after.sql":u}),w=b(A,y),I=f(w),X={title:"DDL API Diffs Suite/Whole Table Changes Samples",component:p},l=h(I),e=l("01-wholly-added-table"),a=l("02-wholly-removed-table"),s=l("03-changed-schema-of-table");var t,o,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:'createCaseStory("01-wholly-added-table")',...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};var c,d,i;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:'createCaseStory("02-wholly-removed-table")',...(i=(d=a.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};var n,_,m;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:'createCaseStory("03-changed-schema-of-table")',...(m=(_=s.parameters)==null?void 0:_.docs)==null?void 0:m.source}}};const L=["Case_01_wholly_added_table","Case_02_wholly_removed_table","Case_03_changed_schema_of_table"];export{e as Case_01_wholly_added_table,a as Case_02_wholly_removed_table,s as Case_03_changed_schema_of_table,L as __namedExportsOrder,X as default};
