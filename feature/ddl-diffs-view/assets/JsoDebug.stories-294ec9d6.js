8a984e4fe752a9916cae2f8bd0934c629bb23725
JayLim2 <JayLim2@users.noreply.github.com>
2026-07-03 11:42:21 +0000

    deploy: 51aa091cbea600b6b49acea23c3c2d5b35dee1fc

---
+++ feature/ddl-diffs-view/assets/JsoDebug.stories-294ec9d6.js
@@ -0,0 +1,15 @@
+import{j as c}from"./_commonjs-dynamic-modules-6308e768.js";import{J as i}from"./AsyncApiOperationViewer-4126e0c9.js";import{p as m}from"./public-api-d6a34651.js";import"./index-f46741a2.js";import"./UxBadge-d0e51bff.js";import"./DdlTableViewer-4c3a3e60.js";import"./index-abe66225.js";import"./DdlTableDiffsViewer-06cb784a.js";/* empty css              */import"./GraphQLOperationDiffViewer-54a4e89c.js";import"./GraphPropNodeViewer-76557377.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-14cec534.js";const y={title:"Debug/Jso Viewer",component:i,parameters:{},argTypes:{jsoText:{control:"text"},componentsText:{control:"text"},source:{control:{disable:!0},table:{disable:!0}}},args:{jsoText:""}},s={args:{jsoText:""},render:o=>{const{jsoText:e,...r}=o,t=l(e);return console.log(e),console.debug("Prepared JSO:",t),c.jsx(i,{...r,source:t,initialLevel:1})}};function l(o){let e;try{e=JSON.parse(o)}catch(r){console.error("Cannot parse JSON:",r),e=void 0}try{e||(e=m(o))}catch(r){console.error("Cannot parse YAML:",r),e=void 0}return(!e||typeof e!="object")&&(e={}),console.debug("Parsed source:",e),e}var n,a,p;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    jsoText: ''
+  },
+  render: args => {
+    const {
+      jsoText,
+      ...viewerArgs
+    } = args;
+    const parsedJso = parseJsonOrYaml(jsoText);
+    console.log(jsoText);
+    console.debug('Prepared JSO:', parsedJso);
+    return <JsoViewer {...viewerArgs} source={parsedJso as object | null} initialLevel={1} />;
+  }
+}`,...(p=(a=s.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};const A=["Debug"];export{s as Debug,A as __namedExportsOrder,y as default};
---
+++ feature/ddl-diffs-view/assets/build-from-ddl-browser-23601a01.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./iframe-6018cafb.js";async function a(r,o){const{buildFromDdl:t}=await i(()=>import("./parser.browser-39bfe387.js"),[],import.meta.url);return t(r,o)}export{a as b};
