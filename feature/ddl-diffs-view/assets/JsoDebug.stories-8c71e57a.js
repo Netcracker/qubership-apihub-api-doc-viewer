8d6a6af78f1f8fd2b9ddf925208dee91cce25195
JayLim2 <JayLim2@users.noreply.github.com>
2026-07-20 14:43:52 +0000

    deploy: cc44470d98406e1ef06bdc8e795f195fa048f583

---
+++ feature/ddl-diffs-view/assets/JsoDebug.stories-8c71e57a.js
@@ -0,0 +1,15 @@
+import{j as c}from"./_commonjs-dynamic-modules-6308e768.js";import{J as i}from"./AsyncApiOperationViewer-09e22ec6.js";import{p as m}from"./public-api-d6a34651.js";import"./index-f46741a2.js";import"./UxBadge-ba9cc67b.js";import"./DdlTableViewer-907af8b6.js";import"./index-eadb8505.js";import"./DdlTableDiffsViewer-e8d390cc.js";/* empty css              */import"./GraphQLOperationDiffViewer-ae9cc0ec.js";import"./GraphPropNodeViewer-24ca20d4.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-08bd138c.js";const y={title:"Debug/Jso Viewer",component:i,parameters:{},argTypes:{jsoText:{control:"text"},componentsText:{control:"text"},source:{control:{disable:!0},table:{disable:!0}}},args:{jsoText:""}},s={args:{jsoText:""},render:o=>{const{jsoText:e,...r}=o,t=l(e);return console.log(e),console.debug("Prepared JSO:",t),c.jsx(i,{...r,source:t,initialLevel:1})}};function l(o){let e;try{e=JSON.parse(o)}catch(r){console.error("Cannot parse JSON:",r),e=void 0}try{e||(e=m(o))}catch(r){console.error("Cannot parse YAML:",r),e=void 0}return(!e||typeof e!="object")&&(e={}),console.debug("Parsed source:",e),e}var n,a,p;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    jsoText: ''
+  },
+  render: args => {
+    const {
+      jsoText,
+      ...viewerArgs
+    } = args;
+    const parsedJso = parseJsonOrYaml(jsoText);
+    console.log(jsoText);
+    console.debug('Prepared JSO:', parsedJso);
+    return <JsoViewer {...viewerArgs} source={parsedJso as object | null} initialLevel={1} />;
+  }
+}`,...(p=(a=s.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};const A=["Debug"];export{s as Debug,A as __namedExportsOrder,y as default};
