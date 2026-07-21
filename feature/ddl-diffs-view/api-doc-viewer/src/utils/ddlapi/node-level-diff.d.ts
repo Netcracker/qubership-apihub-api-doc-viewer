c447a4b377e146ed2999b1e6acc63ec67631ce81
JayLim2 <JayLim2@users.noreply.github.com>
2026-07-21 14:23:58 +0000

    deploy: 031f7f21105da8f1804dc69526262749f1cf2787

--- feature/ddl-diffs-view/api-doc-viewer/src/utils/ddlapi/node-level-diff.d.ts
+++ feature/ddl-diffs-view/api-doc-viewer/src/utils/ddlapi/node-level-diff.d.ts
@@ -1,12 +1,6 @@
 import { TitleRowProps } from '../../components/shared-components/TitleRow/types';
-import { ChangedPropertyMetaData } from '../../../../next-data-model/src/model/abstract/tree-with-diffs/tree-node.interface';
-import { DdlApiColumnRowValue, DdlApiIndexRowValue } from '../../../../next-data-model/src/model/ddlapi/tree/node-value';
-import { DdlApiTreeNodeWithDiffs } from '../../../../next-data-model/src/model/ddlapi/types/aliases';
-import { DdlApiTreeNodeKinds } from '../../../../next-data-model/src/model/ddlapi/types/node-kind';
-import { LayoutSide } from '../../types/internal/LayoutSide';
-/** Row value for DDL property list items rendered with a title row + subheader (column, index). */
-export type DdlApiPropertyRowValue = DdlApiColumnRowValue | DdlApiIndexRowValue;
-export type DdlApiPropertyNodeWithDiffs = DdlApiTreeNodeWithDiffs<typeof DdlApiTreeNodeKinds.COLUMN> | DdlApiTreeNodeWithDiffs<typeof DdlApiTreeNodeKinds.INDEX>;
-export declare function takeNodeDiffIfPresent(node: DdlApiPropertyNodeWithDiffs): ChangedPropertyMetaData | undefined;
+import { DdlApiPropertyNodeWithDiffs, takeDdlPropertyNodeDiffIfPresent } from '../../../../next-data-model/src/model/ddlapi/tree-with-diffs/property-row-diffs';
+export type { DdlApiPropertyNodeWithDiffs, DdlApiPropertyRowValue, } from '../../../../next-data-model/src/model/ddlapi/tree-with-diffs/property-row-diffs';
+export { DDL_COLUMN_FLAG_DIFF_KEYS, DDL_INDEX_FLAG_DIFF_KEYS, } from '../../../../next-data-model/src/model/ddlapi/tree-with-diffs/property-row-diffs';
+export declare const takeNodeDiffIfPresent: typeof takeDdlPropertyNodeDiffIfPresent;
 export declare function buildDdlPropertyTitleRowDiffProps(node: DdlApiPropertyNodeWithDiffs): Pick<TitleRowProps, "diff" | "descendantDiffs" | "diffsSeverities" | "highlightingMode">;
-export declare function isNodeSubheaderVisible(nodeLevelDiff: ChangedPropertyMetaData | undefined, layoutSide: LayoutSide): boolean;
---
+++ feature/ddl-diffs-view/assets/graph-api-transformers-e220c83b.js
@@ -0,0 +1,1 @@
+import{b as r}from"./DiffBadge-03c32659.js";import{b as a}from"./buildASTSchema-f14864f0.js";function t(o){return r(a(o,{noLocation:!0}))}export{t as b};
