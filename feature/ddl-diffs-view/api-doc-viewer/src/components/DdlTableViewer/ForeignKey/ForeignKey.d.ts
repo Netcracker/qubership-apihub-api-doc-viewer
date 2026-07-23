f7ad5090956d6b5c2b7054212acfc9363cc61efd
JayLim2 <JayLim2@users.noreply.github.com>
2026-07-23 21:40:57 +0000

    deploy: 2c74c3e39e99eb6ab7ae84acec6604d2bd238799

--- feature/ddl-diffs-view/api-doc-viewer/src/components/DdlTableViewer/ForeignKey/ForeignKey.d.ts
+++ feature/ddl-diffs-view/api-doc-viewer/src/components/DdlTableViewer/ForeignKey/ForeignKey.d.ts
@@ -1,9 +1,11 @@
 import { DdlApiForeignKeyTarget } from '../../../../../next-data-model/src/model/ddlapi/tree/node-value';
+import { HighlightVariant } from '../../../../../next-data-model/src/model/abstract/tree-with-diffs/tree-node.interface';
 import { FC } from '../../../../../../node_modules/react';
 type ForeignKeyProps = {
     target: DdlApiForeignKeyTarget;
     /** When true, only the navigation link is rendered (FK badge supplied by the caller). */
     hideBadge?: boolean;
+    textHighlighterColor?: Exclude<HighlightVariant, HighlightVariant.Gray>;
 };
 export declare const ForeignKey: FC<ForeignKeyProps>;
 export {};
