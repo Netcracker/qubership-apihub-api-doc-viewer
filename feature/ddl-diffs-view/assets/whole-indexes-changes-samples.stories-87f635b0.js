e3cc03dd7f7d5c5a7f09eba62ec99c0974949a62
JayLim2 <JayLim2@users.noreply.github.com>
2026-07-30 14:14:22 +0000

    deploy: ad91fdbef571e55709f074ffabc742040197086a

---
+++ feature/ddl-diffs-view/assets/whole-indexes-changes-samples.stories-87f635b0.js
@@ -0,0 +1,31 @@
+import{c as d,a as p,d as _,b as l}from"./ddlapi-diffs-utils-1f806b2a.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./DdlTableDiffsViewer-8eddd8e1.js";import"./DiffBadge-225a7fbf.js";import"./index-3b584cdd.js";/* empty css              */import"./build-from-ddl-browser-496a69e0.js";import"./iframe-805abe59.js";import"../sb-preview/runtime.js";import"./ddl-story-realm-utils-c0692776.js";const E=`CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE TABLE public.t (
+  c1 integer,
+  c2 text
+);
+`,m=`CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE TABLE public.t (
+  c1 integer,
+  c2 text
+);
+
+CREATE INDEX idx_t_c1 ON public.t (c1);
+CREATE INDEX idx_t_c2 ON public.t (c2);
+`,w=`CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE TABLE public.t (
+  c1 integer,
+  c2 text
+);
+
+CREATE INDEX idx_t_c1 ON public.t (c1);
+CREATE INDEX idx_t_c2 ON public.t (c2);
+`,x=`CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE TABLE public.t (
+  c1 integer,
+  c2 text
+);
+`,C=Object.assign({"../../../../samples/ddlapi-diffs/whole-indexes-changes/01-add-two-indexes-when-none-present/before.sql":E,"../../../../samples/ddlapi-diffs/whole-indexes-changes/02-remove-two-indexes-when-two-present/before.sql":m}),f=Object.assign({"../../../../samples/ddlapi-diffs/whole-indexes-changes/01-add-two-indexes-when-none-present/after.sql":w,"../../../../samples/ddlapi-diffs/whole-indexes-changes/02-remove-two-indexes-when-two-present/after.sql":x}),S=d(C,f),T=p(S),y={..._,title:"DDL API Diffs Suite/Whole Indexes Changes Samples"},c=l(T),e=c("01-add-two-indexes-when-none-present"),t=c("02-remove-two-indexes-when-two-present");var s,n,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:'createCaseStory("01-add-two-indexes-when-none-present")',...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var r,a,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:'createCaseStory("02-remove-two-indexes-when-two-present")',...(i=(a=t.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const X=["Case_01_add_two_indexes_when_none_present","Case_02_remove_two_indexes_when_two_present"];export{e as Case_01_add_two_indexes_when_none_present,t as Case_02_remove_two_indexes_when_two_present,X as __namedExportsOrder,y as default};
