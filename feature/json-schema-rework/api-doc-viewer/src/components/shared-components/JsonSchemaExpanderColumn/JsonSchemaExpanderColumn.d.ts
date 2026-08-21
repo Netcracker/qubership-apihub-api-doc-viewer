4100b51e8e2c29beb67d249c40a872ad3bfad67b
JayLim2 <JayLim2@users.noreply.github.com>
2026-08-21 09:43:56 +0000

    deploy: 1bd83eb3cef82bd57899dc5fdee107b3f1aaf977

--- feature/json-schema-rework/api-doc-viewer/src/components/shared-components/JsonSchemaExpanderColumn/JsonSchemaExpanderColumn.d.ts
+++ feature/json-schema-rework/api-doc-viewer/src/components/shared-components/JsonSchemaExpanderColumn/JsonSchemaExpanderColumn.d.ts
@@ -1,5 +1,5 @@
 import { FC } from '../../../../../../node_modules/react';
-export declare const JSON_SCHEMA_EXPANDER_COLUMN_WIDTH_CLASS = "w-5 min-w-[20px] shrink-0 flex-none";
+export declare const JSON_SCHEMA_EXPANDER_COLUMN_WIDTH_CLASS = "w-4 min-w-[16px] shrink-0 flex-none";
 export declare const JsonSchemaRootExpanderOffset: FC;
 export type JsonSchemaExpanderColumnProps = {
     isRoot: boolean;
---
+++ feature/json-schema-rework/assets/combiner-samples.stories-c916c3a6.js
@@ -0,0 +1,16 @@
+import{c as a,a as r}from"./json-schema-samples-cases-5c62eccb.js";import{j as s,c as m}from"./json-schema-next-samples-common-e5f7b57d.js";import"./parse-yaml-source-c8480a27.js";import"./public-api-d6a34651.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./AsyncApiOperationViewer-8dcf70e1.js";import"./DiffBadge-7a1cf565.js";import"./IndexesNodeViewer-1fe33aff.js";import"./DdlTableDiffsViewer-349954da.js";/* empty css              */import"./DdlTableViewer-b54d3582.js";import"./GraphQLOperationDiffViewer-9273ef44.js";import"./GraphPropNodeViewer-5195dcd6.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-e2e598a2.js";const p=`oneOf:
+  - type: string
+    description: String variant
+    minLength: 1
+  - type: object
+    description: Object variant
+    properties:
+      prop1:
+        type: string
+      prop2:
+        type: number
+  - anyOf:
+      - type: number
+      - type: boolean
+        description: Boolean variant
+`,i=Object.assign({"../../../../samples/json-schema/combiner/001-oneof-nested-anyof/sample.yaml":p}),c=a(i),y=r(c),l=m(y),F={...s,id:"json-schema-next-combiner",title:"JSON Schema Next/Combiner"},e=l("001-oneof-nested-anyof");var o,t,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:'createCaseStory("001-oneof-nested-anyof")',...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const I=["Case_001_oneof_nested_anyof"];export{e as Case_001_oneof_nested_anyof,I as __namedExportsOrder,F as default};
