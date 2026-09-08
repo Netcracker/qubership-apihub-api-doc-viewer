8de15444eeb2891df265af5c5e68e28e09295678
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-09-08 13:04:35 +0400

    feat: replace button `Show unchanged N nodes` by compact `three-dots-icon`, added horizontal line after LevelIndicator

---
+++ packages/api-doc-viewer/src/components/kit/icons/ThreeDotsIcon.tsx
@@ -0,0 +1,14 @@
+import { FC, memo } from 'react'
+
+export const ThreeDotsIcon: FC = memo(() => {
+  return (
+    <div className="flex" style={{ marginTop: 2 }}>
+      <svg className="three-dots-icon" width="13" height="3" viewBox="0 0 13 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M11.5 0C12.3284 0 13 0.67157 13 1.5C13 2.3284 12.3284 3 11.5 3C10.6716 3 10 2.3284 10 1.5C10 0.67157 10.6716 0 11.5 0ZM6.5 0C7.3284 0 8 0.67157 8 1.5C8 2.3284 7.3284 3 6.5 3C5.67157 3 5 2.3284 5 1.5C5 0.67157 5.67157 0 6.5 0ZM1.5 0C2.32843 0 3 0.67157 3 1.5C3 2.3284 2.32843 3 1.5 3C0.67157 3 0 2.3284 0 1.5C0 0.67157 0.67157 0 1.5 0Z"
+          fill="rgb(0 104 255 / 1)"
+        />
+      </svg>
+    </div>
+  )
+})
--- packages/api-doc-viewer/src/components/shared-components/ShowUnchangedRow/ShowUnchangedRowContent.tsx
+++ packages/api-doc-viewer/src/components/shared-components/ShowUnchangedRow/ShowUnchangedRowContent.tsx
@@ -1,5 +1,8 @@
 import { FC, memo, useCallback } from "react"
-import "../Selector/Selector.css"
+import { NestingHorizontalIndicator } from "@apihub/components/common/NestingHorizontalIndicator"
+import { ThreeDotsIcon } from "@apihub/components/kit/icons/ThreeDotsIcon"
+import { UxTooltip } from "@apihub/components/kit/ux/UxTooltip/UxTooltip"
+import "./ShowUnchangedRowContent.css"
 import { LevelIndicator } from "../LevelIndicator"
 import { ATTRIBUTE_PRECEDED_BY } from "../WithPrecededByProps"
 import type { ShowUnchangedRowProps } from "./ShowUnchangedRow"
@@ -40,17 +43,22 @@ export const ShowUnchangedRowContent: FC<ShowUnchangedRowContentProps> = memo((p
       {level > 0 && (
         <div className="level-indicator-column flex items-stretch self-stretch">
           <LevelIndicator level={level} />
-          <div className="w-4" aria-hidden="true" />
+          <div className="show-unchanged-connector flex w-4 items-center justify-center" aria-hidden="true">
+            <NestingHorizontalIndicator />
+          </div>
         </div>
       )}
       <div className="json-schema-property-row-body flex min-h-[26px] min-w-0 flex-1 items-center">
-        <button
-          type="button"
-          className="button-selector-option button-selector-option_secondary"
-          onClick={onClick}
-        >
-          {formatShowUnchangedLabel(count)}
-        </button>
+        <UxTooltip text={formatShowUnchangedLabel(count)}>
+          <button
+            type="button"
+            className="show-unchanged-trigger"
+            aria-label={formatShowUnchangedLabel(count)}
+            onClick={onClick}
+          >
+            <ThreeDotsIcon />
+          </button>
+        </UxTooltip>
       </div>
     </div>
   )
