e8b191f1164735521d0bc5a6044494d05ea20d2c
JayLim2 <JayLim2@users.noreply.github.com>
2026-09-15 22:59:01 +0000

    deploy: ac79d223ccbd68ee90b0abfbb0e8065d7a239370

---
+++ feature/json-schema-rework/assets/combiner.stories-925f14b2.js
@@ -0,0 +1,16 @@
+import{c as a,a as s}from"./json-schema-samples-cases-65f7b341.js";import{j as r,c as m}from"./json-schema-samples-common-4d1e808f.js";import"./parse-yaml-source-3e95a000.js";import"./public-api-99af098d.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./AsyncApiOperationViewer-8152ae02.js";import"./DiffBadge-f5ba12be.js";import"./IndexesNodeViewer-71f18f3c.js";import"./DdlTableDiffsViewer-c0933c8a.js";/* empty css              */import"./DdlTableViewer-47d4d8fc.js";import"./GraphQLOperationDiffViewer-ec42f7d3.js";import"./GraphPropNodeViewer-452b0f1f.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-c53f5f81.js";const p=`oneOf:
+  - type: string
+    description: String variant
+    minLength: 1
+  - type: object
+    description: Object variant
+    properties:
+      prop1:
+        type: string
+      prop2:
+        type: number
+  - anyOf:
+      - type: number
+      - type: boolean
+        description: Boolean variant
+`,i=Object.assign({"../../../../samples/json-schema/combiner/001-oneof-nested-anyof/sample.yaml":p}),c=a(i),y=s(c),l=m(y),I={...r,id:"json-schema-suite-combiner",title:"JSON Schema Suite/Combiner"},e=l("001-oneof-nested-anyof");var o,t,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:'createCaseStory("001-oneof-nested-anyof")',...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const E=["Case_001_oneof_nested_anyof"];export{e as Case_001_oneof_nested_anyof,E as __namedExportsOrder,I as default};
