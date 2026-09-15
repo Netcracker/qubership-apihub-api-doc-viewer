e0d3223c5712e2e094af8acb281011e1d074136b
JayLim2 <JayLim2@users.noreply.github.com>
2026-09-15 13:50:02 +0000

    deploy: 3c98c6a9d30c9ed902edd21e8c2beca39db14159

---
+++ feature/json-schema-rework/assets/circular-ref.stories-6457c488.js
@@ -0,0 +1,10 @@
+import{j as m}from"./_commonjs-dynamic-modules-6308e768.js";import{e as c}from"./AsyncApiOperationViewer-ea0405c1.js";import{e as p,R as n}from"./preprocess-0750a535.js";import{j as i,t as d}from"./json-schema-samples-common-bbbdec74.js";import"./index-f46741a2.js";import"./DiffBadge-f5ba12be.js";import"./IndexesNodeViewer-f60e1e7d.js";import"./DdlTableDiffsViewer-dbd634ed.js";/* empty css              */import"./DdlTableViewer-706d51e9.js";import"./GraphQLOperationDiffViewer-ec42f7d3.js";import"./GraphPropNodeViewer-452b0f1f.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-c53f5f81.js";import"./public-api-99af098d.js";const l=5,s={type:"object",properties:{a:{$ref:"#/components/schemas/A"},b:{$ref:"#/components/schemas/A"},c:{type:"string"},d:{type:"object",properties:{e:{type:"number"}}}}},o={schemas:{A:{type:"object",properties:{c:{$ref:"#/components/schemas/A"}}}}},S=p({schema:s,target:n,additionalComponents:o,circular:!0}),g={...i,id:"json-schema-suite-circular-ref",title:"JSON Schema Suite/Circular Ref"},e={args:{caseId:"cycled",sampleYaml:d({schema:s,additionalComponents:o})},render:()=>m.jsx(c,{schema:S,expandedDepth:l})};var r,a,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    caseId: "cycled",
+    sampleYaml: toSampleYaml({
+      schema: rawSchema,
+      additionalComponents
+    })
+  },
+  render: () => <JsonSchemaNextViewer schema={cycledSchema} expandedDepth={JSON_SCHEMA_SUITE_EXPANDED_DEPTH} />
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const O=["Cycled"];export{e as Cycled,O as __namedExportsOrder,g as default};
---
+++ feature/json-schema-rework/assets/combiner.stories-0bc374c2.js
@@ -0,0 +1,16 @@
+import{c as a,a as s}from"./json-schema-samples-cases-65f7b341.js";import{j as r,c as m}from"./json-schema-samples-common-bbbdec74.js";import"./parse-yaml-source-3e95a000.js";import"./public-api-99af098d.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./AsyncApiOperationViewer-ea0405c1.js";import"./DiffBadge-f5ba12be.js";import"./IndexesNodeViewer-f60e1e7d.js";import"./DdlTableDiffsViewer-dbd634ed.js";/* empty css              */import"./DdlTableViewer-706d51e9.js";import"./GraphQLOperationDiffViewer-ec42f7d3.js";import"./GraphPropNodeViewer-452b0f1f.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-c53f5f81.js";const p=`oneOf:
+  - type: string
+    description: String variant
+    minLength: 1
+  - type: object
+    description: Object variant
+    properties:
+      prop1:
+        type: string
+      prop2:
+        type: number
+  - anyOf:
+      - type: number
+      - type: boolean
+        description: Boolean variant
+`,i=Object.assign({"../../../../samples/json-schema/combiner/001-oneof-nested-anyof/sample.yaml":p}),c=a(i),y=s(c),l=m(y),I={...r,id:"json-schema-suite-combiner",title:"JSON Schema Suite/Combiner"},e=l("001-oneof-nested-anyof");var o,t,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:'createCaseStory("001-oneof-nested-anyof")',...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const E=["Case_001_oneof_nested_anyof"];export{e as Case_001_oneof_nested_anyof,E as __namedExportsOrder,I as default};
