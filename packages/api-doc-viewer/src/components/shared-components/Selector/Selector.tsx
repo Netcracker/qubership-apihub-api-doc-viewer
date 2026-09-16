6f1ecf8ca136e0efb6c2a6b9368259422254b3a8
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-09-16 16:35:44 +0400

    fix: added wrapping buttons in selector

--- packages/api-doc-viewer/src/components/shared-components/Selector/Selector.tsx
+++ packages/api-doc-viewer/src/components/shared-components/Selector/Selector.tsx
@@ -45,7 +45,7 @@ export function Selector<
   }
 
   return (
-    <div className='flex flex-row gap-2'>
+    <div className='flex flex-row flex-wrap gap-2'>
       {options.map((option) => {
         const { diffsRelatedClassesList, isInvisible } = resolveOptionDiffPresentation({
           diffs: option.diffs,
