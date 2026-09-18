4d25cabf53d06b536e726b0e76dfd3f34c603b97
JayLim2 <JayLim2@users.noreply.github.com>
2026-09-18 21:56:27 +0000

    deploy: 704900683f83c1cd6ce657a37d7283da71a8523b

---
+++ feature/json-schema-screenshots/assets/object-validations-default.stories-28b053dc.js
@@ -0,0 +1,39 @@
+import{c as S}from"./diffs-samples-cases-1df1f3ae.js";import{c as C,d as v,j as D,b as O}from"./json-schema-diffs-utils-5be2490a.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./AsyncApiOperationViewer-dc7eb465.js";import"./DiffBadge-2c39fcfb.js";import"./IndexesNodeViewer-9cab8d43.js";import"./DdlTableDiffsViewer-4ca2d74c.js";/* empty css              */import"./DdlTableViewer-01b667c9.js";import"./GraphQLOperationDiffViewer-89ad93c7.js";import"./GraphPropNodeViewer-3d0fd400.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-f5bfa916.js";import"./preprocess-8cff05ac.js";import"./parse-yaml-source-3e95a000.js";import"./public-api-99af098d.js";import"./combiner-changed-variant-43f703e4.js";const J=`type: object
+
+`,F=`type: object
+default: {}
+
+`,V=`type: object
+default: {}
+
+`,x=`type: object
+default:
+  id: alpha
+  count: 1
+
+`,B=`type: object
+default:
+  id: alpha
+  count: 1
+
+`,I=`type: object
+default: {}
+
+`,T=`type: object
+
+`,A=`type: object
+default:
+  id: alpha
+  count: 1
+
+`,E=`type: object
+default: {}
+
+`,N=`type: object
+default:
+  id: beta
+  count: 2
+  active: true
+  label: gamma
+
+`,R=Object.assign({"../../../../samples/json-schema-diffs/type-changes/object-validations/default/001-default-added-empty-object/before.yaml":J,"../../../../samples/json-schema-diffs/type-changes/object-validations/default/002-default-removed-empty-object/before.yaml":F,"../../../../samples/json-schema-diffs/type-changes/object-validations/default/003-default-replaced-empty-to-object/before.yaml":V,"../../../../samples/json-schema-diffs/type-changes/object-validations/default/004-default-replaced-object-to-empty/before.yaml":x,"../../../../samples/json-schema-diffs/type-changes/object-validations/default/005-default-replaced-object-to-object/before.yaml":B}),W=Object.assign({"../../../../samples/json-schema-diffs/type-changes/object-validations/default/001-default-added-empty-object/after.yaml":I,"../../../../samples/json-schema-diffs/type-changes/object-validations/default/002-default-removed-empty-object/after.yaml":T,"../../../../samples/json-schema-diffs/type-changes/object-validations/default/003-default-replaced-empty-to-object/after.yaml":A,"../../../../samples/json-schema-diffs/type-changes/object-validations/default/004-default-replaced-object-to-empty/after.yaml":E,"../../../../samples/json-schema-diffs/type-changes/object-validations/default/005-default-replaced-object-to-object/after.yaml":N}),k=S(R,W),q=C(k),se={title:"JSON Schema Diffs Suite/Object Validation/Object Validations Default",component:v,argTypes:D},e=O(v,q),t=e("001-default-added-empty-object"),a=e("002-default-removed-empty-object"),o=e("003-default-replaced-empty-to-object"),s=e("004-default-replaced-object-to-empty"),c=e("005-default-replaced-object-to-object");var r,d,l;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:'createCaseStory("001-default-added-empty-object")',...(l=(d=t.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var p,m,n;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:'createCaseStory("002-default-removed-empty-object")',...(n=(m=a.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var _,f,i;o.parameters={...o.parameters,docs:{...(_=o.parameters)==null?void 0:_.docs,source:{originalSource:'createCaseStory("003-default-replaced-empty-to-object")',...(i=(f=o.parameters)==null?void 0:f.docs)==null?void 0:i.source}}};var b,j,u;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:'createCaseStory("004-default-replaced-object-to-empty")',...(u=(j=s.parameters)==null?void 0:j.docs)==null?void 0:u.source}}};var y,g,h;c.parameters={...c.parameters,docs:{...(y=c.parameters)==null?void 0:y.docs,source:{originalSource:'createCaseStory("005-default-replaced-object-to-object")',...(h=(g=c.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};const ce=["Case_001_default_added_empty_object","Case_002_default_removed_empty_object","Case_003_default_replaced_empty_to_object","Case_004_default_replaced_object_to_empty","Case_005_default_replaced_object_to_object"];export{t as Case_001_default_added_empty_object,a as Case_002_default_removed_empty_object,o as Case_003_default_replaced_empty_to_object,s as Case_004_default_replaced_object_to_empty,c as Case_005_default_replaced_object_to_object,ce as __namedExportsOrder,se as default};
