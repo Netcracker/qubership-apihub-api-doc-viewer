9a3927dbe113070730b146cbc06e2352823a2ea3
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-09-23 20:22:17 +0400

    Revert "fix: TEMP fix `hideUnchangedNodes = false` in `JsoDiffsViewer`"
    This reverts commit d457f17e18ae822534580e8ffb30b9cd0b03e484.

--- packages/api-doc-viewer/src/components/JsoViewer/embedding/jso-embedding-types.ts
+++ packages/api-doc-viewer/src/components/JsoViewer/embedding/jso-embedding-types.ts
@@ -20,7 +20,6 @@ export type JsoEmbeddedSchemaDiffsComponentProps = {
   diffMetaKeys: DiffMetaKeys
   diffTypes?: ReadonlyArray<DiffType>
   customizationOptions?: CustomizationOptions
-  hideUnchangedNodes?: boolean
 }
 
 export type JsoEmbeddedSchemaDiffsComponent = FC<JsoEmbeddedSchemaDiffsComponentProps>
