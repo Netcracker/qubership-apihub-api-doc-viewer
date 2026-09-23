a76704972004b1d569d48f328b3a368e9cac49c6
JayLim2 <JayLim2@users.noreply.github.com>
2026-09-23 15:08:22 +0000

    deploy: c08671959fd03ef9cb5c8ad05aa65d0add39f4c7

---
+++ feature/json-schema-tests/assets/existing-2-json-schema.stories-ce7cfc0c.js
@@ -0,0 +1,156 @@
+import{c as te}from"./diffs-samples-cases-1df1f3ae.js";import{c as re,J as ne,j as oe,a as me}from"./json-schema-diffs-utils-d36cbe43.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./AsyncApiOperationViewer-12880bbf.js";import"./DiffBadge-4d4a1c37.js";import"./IndexesNodeViewer-6fcd5e03.js";import"./DdlTableDiffsViewer-d78f4219.js";/* empty css              */import"./DdlTableViewer-d47ea043.js";import"./GraphQLOperationDiffViewer-449194d6.js";import"./GraphPropNodeViewer-9f9c17b4.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-b5e727b5.js";import"./preprocess-71f62f03.js";import"./parse-yaml-source-0cc43eab.js";import"./public-api-99af098d.js";const ie=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-internal: true
+`,ce=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-metadata:
+  owner: 'platform-team'
+`,pe=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-tags:
+  - 'beta'
+  - 'internal'
+`,_e=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,de=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,le=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,ge=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,he=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,ye=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,ve=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,xe=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,fe=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,ue=`type: 'string'
+`,je=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,be=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+`,Ce=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+`,Se=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+`,Le=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+`,Je=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema: true
+`,De=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  owner: 'platform-team'
+`,Oe=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  - 'beta'
+  - 'internal'
+`,we=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'boolean'
+`,Ee=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-internal: true
+`,Fe=`type: 'string'
+x-value-schema: true
+`,Be=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema: true
+x-metadata:
+  owner: 'platform-team'
+`,Ie=`type: 'string'
+x-value-schema: true
+x-response-schema:
+  owner: 'platform-team'
+`,Ne=`type: 'string'
+x-value-schema:
+  type: 'integer'
+  minimum: 0
+x-response-schema:
+  type: 'string'
+  minLength: 1
+`,Te=`type: 'string'
+`,Ae=Object.assign({"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/01-primitive-removed/before.yaml":ie,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/02-object-removed/before.yaml":ce,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/03-array-removed/before.yaml":pe,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/04-json-schema-removed/before.yaml":_e,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/05-replaced-to-primitive/before.yaml":de,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/06-replaced-to-object/before.yaml":le,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/07-replaced-to-array/before.yaml":ge,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/08-replaced-to-json-schema/before.yaml":he,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/09-added-and-removed/before.yaml":ye,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/10-removed-and-replaced/before.yaml":ve,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/11-added-and-replaced/before.yaml":xe,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/12-both-replaced/before.yaml":fe,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/13-both-added/before.yaml":ue,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/14-both-removed/before.yaml":je}),Re=Object.assign({"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/01-primitive-removed/after.yaml":be,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/02-object-removed/after.yaml":Ce,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/03-array-removed/after.yaml":Se,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/04-json-schema-removed/after.yaml":Le,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/05-replaced-to-primitive/after.yaml":Je,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/06-replaced-to-object/after.yaml":De,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/07-replaced-to-array/after.yaml":Oe,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/08-replaced-to-json-schema/after.yaml":we,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/09-added-and-removed/after.yaml":Ee,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/10-removed-and-replaced/after.yaml":Fe,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/11-added-and-replaced/after.yaml":Be,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/12-both-replaced/after.yaml":Ie,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/13-both-added/after.yaml":Ne,"../../../../samples/json-schema-diffs/extensions/existing-2-json-schema/14-both-removed/after.yaml":Te}),ke=te(Ae,Re),qe=re(ke),ns={title:"JSON Schema Diffs Suite (Extensions)/Existing 2 JSON Schema",component:ne,argTypes:oe},e=me(ne,qe),s=e("01-primitive-removed"),a=e("02-object-removed"),n=e("03-array-removed"),t=e("04-json-schema-removed"),r=e("05-replaced-to-primitive"),o=e("06-replaced-to-object"),m=e("07-replaced-to-array"),i=e("08-replaced-to-json-schema"),c=e("09-added-and-removed"),p=e("10-removed-and-replaced"),_=e("11-added-and-replaced"),d=e("12-both-replaced"),l=e("13-both-added"),g=e("14-both-removed");var h,y,v;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:'createCaseStory("01-primitive-removed")',...(v=(y=s.parameters)==null?void 0:y.docs)==null?void 0:v.source}}};var x,f,u;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:'createCaseStory("02-object-removed")',...(u=(f=a.parameters)==null?void 0:f.docs)==null?void 0:u.source}}};var j,b,C;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:'createCaseStory("03-array-removed")',...(C=(b=n.parameters)==null?void 0:b.docs)==null?void 0:C.source}}};var S,L,J;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:'createCaseStory("04-json-schema-removed")',...(J=(L=t.parameters)==null?void 0:L.docs)==null?void 0:J.source}}};var D,O,w;r.parameters={...r.parameters,docs:{...(D=r.parameters)==null?void 0:D.docs,source:{originalSource:'createCaseStory("05-replaced-to-primitive")',...(w=(O=r.parameters)==null?void 0:O.docs)==null?void 0:w.source}}};var E,F,B;o.parameters={...o.parameters,docs:{...(E=o.parameters)==null?void 0:E.docs,source:{originalSource:'createCaseStory("06-replaced-to-object")',...(B=(F=o.parameters)==null?void 0:F.docs)==null?void 0:B.source}}};var I,N,T;m.parameters={...m.parameters,docs:{...(I=m.parameters)==null?void 0:I.docs,source:{originalSource:'createCaseStory("07-replaced-to-array")',...(T=(N=m.parameters)==null?void 0:N.docs)==null?void 0:T.source}}};var A,R,k;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:'createCaseStory("08-replaced-to-json-schema")',...(k=(R=i.parameters)==null?void 0:R.docs)==null?void 0:k.source}}};var q,z,G;c.parameters={...c.parameters,docs:{...(q=c.parameters)==null?void 0:q.docs,source:{originalSource:'createCaseStory("09-added-and-removed")',...(G=(z=c.parameters)==null?void 0:z.docs)==null?void 0:G.source}}};var H,K,M;p.parameters={...p.parameters,docs:{...(H=p.parameters)==null?void 0:H.docs,source:{originalSource:'createCaseStory("10-removed-and-replaced")',...(M=(K=p.parameters)==null?void 0:K.docs)==null?void 0:M.source}}};var P,Q,U;_.parameters={..._.parameters,docs:{...(P=_.parameters)==null?void 0:P.docs,source:{originalSource:'createCaseStory("11-added-and-replaced")',...(U=(Q=_.parameters)==null?void 0:Q.docs)==null?void 0:U.source}}};var V,W,X;d.parameters={...d.parameters,docs:{...(V=d.parameters)==null?void 0:V.docs,source:{originalSource:'createCaseStory("12-both-replaced")',...(X=(W=d.parameters)==null?void 0:W.docs)==null?void 0:X.source}}};var Y,Z,$;l.parameters={...l.parameters,docs:{...(Y=l.parameters)==null?void 0:Y.docs,source:{originalSource:'createCaseStory("13-both-added")',...($=(Z=l.parameters)==null?void 0:Z.docs)==null?void 0:$.source}}};var ee,se,ae;g.parameters={...g.parameters,docs:{...(ee=g.parameters)==null?void 0:ee.docs,source:{originalSource:'createCaseStory("14-both-removed")',...(ae=(se=g.parameters)==null?void 0:se.docs)==null?void 0:ae.source}}};const ts=["Case_01_primitive_removed","Case_02_object_removed","Case_03_array_removed","Case_04_json_schema_removed","Case_05_replaced_to_primitive","Case_06_replaced_to_object","Case_07_replaced_to_array","Case_08_replaced_to_json_schema","Case_09_added_and_removed","Case_10_removed_and_replaced","Case_11_added_and_replaced","Case_12_both_replaced","Case_13_both_added","Case_14_both_removed"];export{s as Case_01_primitive_removed,a as Case_02_object_removed,n as Case_03_array_removed,t as Case_04_json_schema_removed,r as Case_05_replaced_to_primitive,o as Case_06_replaced_to_object,m as Case_07_replaced_to_array,i as Case_08_replaced_to_json_schema,c as Case_09_added_and_removed,p as Case_10_removed_and_replaced,_ as Case_11_added_and_replaced,d as Case_12_both_replaced,l as Case_13_both_added,g as Case_14_both_removed,ts as __namedExportsOrder,ns as default};
