399120021f516bac27bf5ccf220ef1d2174249d6
JayLim2 <JayLim2@users.noreply.github.com>
2026-09-24 22:14:43 +0000

    deploy: 95abd612711f81d7c357988192071a622803f03a

---
+++ feature/json-schema-review/assets/anyof-array-two-items.stories-cb5b3c4b.js
@@ -0,0 +1,27 @@
+import{c as p}from"./diffs-samples-cases-1df1f3ae.js";import{c as d,d as i,j as y,a as f}from"./json-schema-diffs-utils-0f350307.js";import"./_commonjs-dynamic-modules-6308e768.js";import"./index-f46741a2.js";import"./AsyncApiOperationViewer-51fb571b.js";import"./UxBadge-3d9cd0ec.js";import"./IndexesNodeViewer-bc39d3de.js";import"./DdlTableDiffsViewer-5f4cf09a.js";/* empty css              */import"./DdlTableViewer-30ab278b.js";import"./GraphQLOperationDiffViewer-a56ad3af.js";import"./GraphPropNodeViewer-0af21220.js";import"./index-415bee12.js";import"./GraphQLOperationViewer-19ba9549.js";import"./preprocess-8acbbf55.js";import"./parse-yaml-source-3e95a000.js";import"./public-api-99af098d.js";import"./combiner-changed-variant-43f703e4.js";const _=`anyOf:
+  - type: array
+    items:
+      - type: string
+  - type: boolean
+`,l=`anyOf:
+  - type: array
+    items:
+      - type: string
+      - type: integer
+        x-internal: true
+        x-version: '1.0.0'
+  - type: boolean
+`,S=`anyOf:
+  - type: array
+    items:
+      - type: string
+      - type: integer
+        x-internal: true
+        x-version: '1.0.0'
+  - type: boolean
+`,g=`anyOf:
+  - type: array
+    items:
+      - type: string
+  - type: boolean
+`,b=Object.assign({"../../../../samples/json-schema-diffs/extensions/anyof-array-two-items/01-second-item-added/before.yaml":_,"../../../../samples/json-schema-diffs/extensions/anyof-array-two-items/02-second-item-removed/before.yaml":l}),v=Object.assign({"../../../../samples/json-schema-diffs/extensions/anyof-array-two-items/01-second-item-added/after.yaml":S,"../../../../samples/json-schema-diffs/extensions/anyof-array-two-items/02-second-item-removed/after.yaml":g}),x=p(b,v),h=d(x),G={title:"JSON Schema Diffs Suite (Extensions)/Node Diff to Extension Inheritance/AnyOf Array Two Items",component:i,argTypes:y},c=f(i,h),e=c("01-second-item-added"),t=c("02-second-item-removed");var s,o,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:'createCaseStory("01-second-item-added")',...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var n,r,m;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:'createCaseStory("02-second-item-removed")',...(m=(r=t.parameters)==null?void 0:r.docs)==null?void 0:m.source}}};const H=["Case_01_second_item_added","Case_02_second_item_removed"];export{e as Case_01_second_item_added,t as Case_02_second_item_removed,H as __namedExportsOrder,G as default};
